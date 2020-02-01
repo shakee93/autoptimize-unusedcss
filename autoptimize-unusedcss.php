5e61cf1930bea26e9be49b85ed0cf2e9f3c862ea
shakee93 <shakee.zats@gmail.com>
2020-02-01 10:52:42 +0530

    added guzzle api

--- autoptimize-unusedcss.php
+++ autoptimize-unusedcss.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Autoptimize UnusedCSS
+Plugin Name: Autoptimize UnusedCSS Power-Up
 Plugin URI:  unusedcss.io
 Description: Removes Unused CSS from your website pages.
 Version:     1.0.0
@@ -11,6 +11,13 @@ Author URI:  http://shakee93.me/
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
+}
+
+if (is_multisite()) {
+    $blog_id = get_current_blog_id();
+    define( 'AO_UUCSS_DIR', WP_CONTENT_DIR . '/uploads/ao_uucss/' . $blog_id . '/' );
+} else {
+    define( 'AO_UUCSS_DIR', WP_CONTENT_DIR . '/uploads/ao_uucss/' );
 }
 
 require('vendor/autoload.php');
--- includes/UnusedCSS_Api.php
+++ includes/UnusedCSS_Api.php
@@ -4,6 +4,7 @@
 class UnusedCSS_Api
 {
 
+    public $apiUrl = 'http://127.0.0.1:9300/';
     public $client;
 
     /**
@@ -12,5 +13,16 @@ class UnusedCSS_Api
     public function __construct()
     {
         $this->client = new GuzzleHttp\Client();
+    }
+
+    public function get($url)
+    {
+        $response = $this->client->get($this->apiUrl . '?url=' . $url);
+
+        if ($response->getStatusCode() == 200) {
+            return $response;
+        }
+
+        return null;
     }
 }
