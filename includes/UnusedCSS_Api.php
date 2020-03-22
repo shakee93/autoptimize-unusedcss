1304b63e417104b9ea4e0f1c70a9ad553cfe4b89
shakee93 <shakee.zats@gmail.com>
2020-03-22 14:29:24 +0530

    fixed api server error handling

--- includes/UnusedCSS_Api.php
+++ includes/UnusedCSS_Api.php
@@ -5,6 +5,7 @@ class UnusedCSS_Api
 {
 
     public $apiUrl = 'https://unusedcss.herokuapp.com';
+    public $apiUrl_debug = 'http://localhost:9300/';
 
     /**
      * UnusedCSS_Api constructor.
@@ -21,21 +22,18 @@ class UnusedCSS_Api
             $appender = '&';
         }
 
-        $url = $this->apiUrl . '?url=' . urlencode($url . $appender . 'doing_unused_fetch=true');
+        $url = $this->apiUrl_debug . '?url=' . urlencode($url . $appender . 'doing_unused_fetch=true');
 
-        // if (strpos($url, 'doing_unused_fetch=true') !== true) {
-        //     $url = urlencode($url . $appender . 'doing_unused_fetch=true');
-        // }
-
-        uucss_log($url);
-        
         $response = wp_remote_get($url);
- 
-        if ( is_array( $response ) && ! is_wp_error( $response ) ) {
-            $headers = $response['headers'];
-            $body    = $response['body'];
 
-            return json_decode($body);
+        if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+
+            if($response['response']['code'] == 200) {
+                $body    = $response['body'];
+                return json_decode($body);
+            }
+
+            return null;
         }
         else {
             return null;
