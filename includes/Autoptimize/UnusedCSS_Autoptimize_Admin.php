ed62585c3f1ac957ede0ee3307e874d3a75da4c2
shakee93 <shakee.zats@gmail.com>
2020-04-01 22:09:56 +0530

    change ao tab order and send to last

--- includes/Autoptimize/UnusedCSS_Autoptimize_Admin.php
+++ includes/Autoptimize/UnusedCSS_Autoptimize_Admin.php
@@ -22,7 +22,7 @@ class UnusedCSS_Autoptimize_Admin {
         $this->ao_uucss = $ao_uucss;
 
         add_action( 'admin_menu', array( $this, 'add_ao_page' ) );
-        add_filter( 'autoptimize_filter_settingsscreen_tabs', [$this, 'add_ao_tab'], 10, 1 );
+        add_filter( 'autoptimize_filter_settingsscreen_tabs', [$this, 'add_ao_tab'], 20, 1 );
 
         add_action('admin_init', function () {
 
