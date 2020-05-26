2e7882a703ed3b0e7f2d33fb2940ec397d358319
shakee93 <shakee.zats@gmail.com>
2020-05-26 15:23:51 +0530

    removed old standalone api

--- includes/UnusedCSS_Api.php
+++ includes/UnusedCSS_Api.php
@@ -9,8 +9,6 @@ class UnusedCSS_Api
 
     public $options = [];
 
-    public $legacyApi = true;
-
     /**
      * UnusedCSS_Api constructor.
      */
@@ -19,15 +17,11 @@ class UnusedCSS_Api
         if (defined('UUCSS_API_URL')) {
             $this->apiUrl = UUCSS_API_URL;
         }
-
-	    if (defined('UUCSS_NEXT')) {
-		    $this->legacyApi = false;
-	    }
     }
 
 	public function get($url) {
 
-	    $response = ($this->legacyApi) ? $this->legacyApi( $url ) : $this->api($url);
+	    $response = $this->api($url);
 
         if ( is_array( $response ) && ! is_wp_error( $response ) ) {
 
@@ -57,19 +51,6 @@ class UnusedCSS_Api
 			'headers' => [
 				'Authorization' => 'Bearer b2729838c86b48be8d3b03da2cd1c531'
 			]
-		]);
-	}
-
-	function legacyApi($url) {
-		$args = [
-			'url' => urlencode($url),
-			'options' => $this->options
-		];
-
-		$url = $this->apiUrl . '?' . http_build_query($args);
-
-		return wp_remote_get($url, [
-			'timeout' => 20
 		]);
 	}
 
