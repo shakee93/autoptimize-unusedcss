8ea6bac6534abbc0f102f5b314ea72b835451cc0
shakee93 <shakee.zats@gmail.com>
2020-06-23 13:47:08 +0530

    added settings page to plugin action links

--- autoptimize-unusedcss.php
+++ autoptimize-unusedcss.php
@@ -3,7 +3,7 @@
 Plugin Name: Autoptimize UnusedCSS Power-Up
 Plugin URI:  unusedcss.io
 Description: Removes Unused CSS from your website pages.
-Version:     1.0.0
+Version:     0.0.1
 Author:      Shakeeb Sadikeen
 Author URI:  https://shakeeb.dev/
 */
@@ -16,11 +16,21 @@ define( 'UUCSS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'UUCSS_PLUGIN_FILE', __FILE__ );
 define( 'UUCSS_DEBUG', true );
 
-require('vendor/autoload.php');
+require( 'vendor/autoload.php' );
 
-add_action('plugins_loaded', function () {
+
+add_action( 'plugins_loaded', function () {
 
 	$ao_uucss = new UnusedCSS_Autoptimize();
-	new UnusedCSS_Autoptimize_Admin($ao_uucss);
+	new UnusedCSS_Autoptimize_Admin( $ao_uucss );
 
-});
+} );
+
+
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), function ( $links ) {
+	$_links = array(
+		'<a href="' . admin_url( 'options-general.php?page=uucss' ) . '">Settings</a>',
+	);
+
+	return array_merge( $_links, $links );
+} );
