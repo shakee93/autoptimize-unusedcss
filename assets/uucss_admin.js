a503845b48470979668ace4f47c8bd0b98a96d99
shakee93 <shakee.zats@gmail.com>
2020-09-28 11:33:21 +0530

    feat: removed unused verify api key js function

--- assets/uucss_admin.js
+++ assets/uucss_admin.js
@@ -6,47 +6,7 @@
         // options page
         window.tagBox.init();
 
-        var $status = $('#verification_status')
         var $input = $('#uucss_api_key')
-        var $verified = $('input[name="autoptimize_uucss_settings[uucss_api_key_verified]"]')
-        var $packs = $('input[name="autoptimize_uucss_settings[uucss_whitelist_packs]"]')
-
-        function verifyApiKey() {
-
-            $verified.val(undefined)
-            if ($input.val().length === 0) {
-                $status.removeClass().text('please fill your api key here !');
-                return;
-            }
-
-            $status.text('loading...');
-
-            wp.ajax.post('verify_api_key',
-                {
-                    api_key: $input.val(),
-                    url: uucss.url,
-                }
-                ).done(function (response) {
-
-                $verified.val('1')
-                $status.text('verified !').removeClass().addClass('success')
-
-                // autoload whitelist packs
-                if ($('[name="autoptimize_uucss_settings[uucss_api_key_verified]"]').val() === '1' && $('#whitelist_packs').select2('data').length === 0) {
-
-                    $('#uucss-pack-suggest').trigger('click')
-                }
-
-            }).fail(function () {
-                $verified.val(undefined)
-                $status.text('failed !').removeClass().addClass('failed')
-
-            });
-
-        }
-
-        // verifyApiKey();
-        // $input.on('input', verifyApiKey)
 
         try {
             var _url = new URL(window.location.href);
