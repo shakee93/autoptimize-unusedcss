7fc183b80d8a6d9646cfde91634b2fccdf9490a0
Azeez <azeezs2012@gmail.com>
2020-10-07 17:11:32 +0530

    change file structure on boarding

--- autoptimize-unusedcss.php
+++ autoptimize-unusedcss.php
@@ -30,6 +30,7 @@ require( 'vendor/autoload.php' );
 
 add_action( 'plugins_loaded', function () {
 
+    new UnusedCSS_Autoptimize_Onboard();
 	$ao_uucss = new UnusedCSS_Autoptimize();
 	new UnusedCSS_Autoptimize_Admin( $ao_uucss );
 
@@ -42,51 +43,5 @@ add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), function ( $li
 
 	return array_merge( $_links, $links );
 } );
-
-register_activation_hook(UUCSS_PLUGIN_FILE, function(){
-    add_option('uucss_do_activation_redirect', true);
-});
-
-add_action('admin_init', 'uucss_redirect');
-
-function uucss_redirect() {
-    if (get_option('uucss_do_activation_redirect', false)) {
-        delete_option('uucss_do_activation_redirect');
-        wp_redirect('/wp-admin/options-general.php?page=uucss-onboarding');
-    }
-}
-
-function uucss_on_boarding_page(){
-    wp_enqueue_script('post');
-    ?>
-    <div class="uucss-on-board">
-        <h2>My Plugin Page Title</h2>
-        <div class="footer">
-            <span><a href="<?php echo admin_url() ?>">Skip</a></span>
-            <span><a class="js-install-ao" href="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=autoptimize' ) ?>" target="_blank">Install</a></span>
-            <span><a class="js-install-ao" href="<?php echo UnusedCSS_Utils::activate_plugin( 'autoptimize/autoptimize.php' ) ?>" target="_blank">Activate</a></span>
-        </div>
-        <div class="uucss-on-board-popup">
-            <div class="uucss-popup-model">
-                <div class="on-board-popup-content">
-                    <iframe id="on-board-popup-frame" src="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=autoptimize' ) ?>" frameborder="0" width="500" height="450"></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-}
-
-function uucss_register_on_board_page() {
-    add_options_page(
-        'UnusedCSS',
-        'UnusedCSS',
-        'manage_options',
-        'uucss-onboarding',
-        'uucss_on_boarding_page'
-    );
-}
-
-add_action('admin_menu', 'uucss_register_on_board_page');
 
 
---
+++ includes/Autoptimize/UnusedCSS_Autoptimize_Onboard.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Class UnusedCSS
+ */
+
+class UnusedCSS_Autoptimize_Onboard
+{
+    public function __construct() {
+
+        register_activation_hook(UUCSS_PLUGIN_FILE, [$this,'register_plugin_activation_hook']);
+
+        add_action('admin_menu', [$this,'uucss_register_on_board_page']);
+
+        add_action('admin_init', [$this,'uucss_redirect']);
+
+        add_action( "wp_ajax_activate_ao_plugin", [ $this, 'activate_ao_plugin' ] );
+
+    }
+
+    function activate_ao_plugin(){
+        $active_plugins = get_option('active_plugins');
+        if (isset($active_plugins['autoptimize/autoptimize.php']))
+            return;
+
+    }
+
+    function uucss_on_boarding_page(){
+        wp_enqueue_script('post');
+        ?>
+        <div class="uucss-on-board">
+            <h2>My Plugin Page Title</h2>
+            <div class="footer">
+                <span><a href="<?php echo admin_url() ?>">Skip</a></span>
+                <span><a class="js-install-ao" href="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=autoptimize' ) ?>" target="_blank">Install</a></span>
+                <span><a class="js-install-ao" href="<?php echo UnusedCSS_Utils::activate_plugin( 'autoptimize/autoptimize.php' ) ?>" target="_blank">Activate</a></span>
+            </div>
+            <div class="uucss-on-board-popup">
+                <div class="uucss-popup-model">
+                    <div class="on-board-popup-content">
+                        <iframe id="on-board-popup-frame" src="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=autoptimize' ) ?>" frameborder="0" width="500" height="450"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+
+    function uucss_redirect() {
+        if (get_option('uucss_do_activation_redirect', false)) {
+            delete_option('uucss_do_activation_redirect');
+            wp_redirect('/wp-admin/options-general.php?page=uucss-onboarding');
+        }
+    }
+
+    function register_plugin_activation_hook(){
+        add_option('uucss_do_activation_redirect', true);
+    }
+
+    function uucss_register_on_board_page() {
+        add_options_page(
+            'UnusedCSS',
+            'UnusedCSS',
+            'manage_options',
+            'uucss-onboarding',
+            [$this, 'uucss_on_boarding_page']
+        );
+    }
+}
