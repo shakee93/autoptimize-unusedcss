e1cbb0aab517a19941ea53badbb4b2b33f8f5df7
Azeez <azeezs2012@gmail.com>
2020-10-12 13:42:09 +0530

    updating steps

--- includes/Autoptimize/parts/onboarding.php
+++ includes/Autoptimize/parts/onboarding.php
@@ -13,13 +13,16 @@
                         'Run First Job'
                     ];
 
-                    if(is_plugin_active('autoptimize/autoptimize.php')){
+                    if(class_exists('autoptimizeOptionWrapper') &&
+                        autoptimizeOptionWrapper::get_option( 'autoptimize_css' ) == "" ){
                         $step = 2;
-                    }
-                    if( is_plugin_active('autoptimize/autoptimize.php') && class_exists('autoptimizeOptionWrapper')){
-                        if(autoptimizeOptionWrapper::get_option( 'autoptimize_css' ) != ""){
-                            $step = 3;
-                        }
+                    }else if(
+                            is_plugin_active('autoptimize/autoptimize.php') &&
+                            class_exists('autoptimizeOptionWrapper') &&
+                            autoptimizeOptionWrapper::get_option( 'autoptimize_css' ) == "on" &&
+                            !UnusedCSS_Autoptimize_Admin::is_api_key_verified()
+                    ){
+                        $step = 3;
                     }else if(UnusedCSS_Autoptimize_Admin::is_api_key_verified()){
                         $step = 4;
                     }
@@ -39,11 +42,11 @@
                         echo sprintf('transform: translate3d(%spx, 0px, 0px); transition-duration: 0.5s;',(($step - 1) * 223 * -1));
                     }
                 ?>">
-                    <div class="actions slide-content">
+                    <div class="actions slide-content <?php echo is_plugin_active('autoptimize/autoptimize.php') ? 'done' : null ?>">
                         <img src="<?php echo UUCSS_PLUGIN_URL . 'assets/wp-rocket-hosting.webp'?>" alt="">
                         <h2>Autoptimize install and Activate</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
-                        <a class="act-button js-activate-ao <?php echo is_plugin_active('autoptimize/autoptimize.php') ? 'done' : null ?>"
+                        <a class="act-button js-activate-ao"
                            href="<?php
                             if(file_exists(ABSPATH . PLUGINDIR . '/autoptimize/autoptimize.php')){
                                 echo UnusedCSS_Utils::activate_plugin( 'autoptimize/autoptimize.php' );
@@ -56,17 +59,25 @@
                         Install  & Activate
                         </a>
                     </div>
-                    <div class="actions slide-content">
+                    <div class="actions slide-content <?php
+                        if(class_exists('autoptimizeOptionWrapper') && autoptimizeOptionWrapper::get_option( 'autoptimize_css' ) == "on") {
+                            echo 'done';
+                        }
+                    ?>">
                         <img src="<?php echo UUCSS_PLUGIN_URL . 'assets/wp-rocket-hosting.webp'?>" alt="">
                         <h2>Configure Autoptimize</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
-                        <a class="act-button js-enable-css-ao" href="<?php echo admin_url('/options-general.php?page=autoptimize') ?>" target="_blank">Configure</a>
+                        <a class="act-button js-enable-css-ao " href="<?php echo admin_url('/options-general.php?page=autoptimize') ?>" target="_blank">Configure</a>
                     </div>
-                    <div class="actions slide-content">
+                    <div class="actions slide-content <?php
+                        if(UnusedCSS_Autoptimize_Admin::is_api_key_verified()){
+                            echo 'done';
+                        }
+                    ?>">
                         <img src="<?php echo UUCSS_PLUGIN_URL . 'assets/wp-rocket-hosting.webp'?>" alt="">
                         <h2>Connect</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
-                        <span><a class="act-button js-uucss-connect" href="<?php echo UnusedCSS_Utils::activation_url("authorize") ?>" target="_blank">Connect</a></span>
+                        <span><a class="act-button js-uucss-connect " href="<?php echo UnusedCSS_Utils::activation_url("authorize") ?>" target="_blank">Connect</a></span>
                     </div>
                     <div class="actions slide-content">
                         <img src="<?php echo UUCSS_PLUGIN_URL . 'assets/wp-rocket-hosting.webp'?>" alt="">
