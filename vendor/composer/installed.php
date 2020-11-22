f59d443fc8000c09ddc9ce60356f0e35a0bccf10
shakee93 <shakee.zats@gmail.com>
2020-11-23 00:58:55 +0530

    fix: add missed composer autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+	'root'     =>
+		array(
+			'pretty_version' => 'dev-master',
+			'version'        => 'dev-master',
+			'aliases'        =>
+				array(),
+			'reference'      => '8e9e4fbaf07a4b85a5e54e2eb4deaa2ee93ccc2b',
+			'name'           => '__root__',
+		),
+	'versions' =>
+		array(
+			'__root__'                    =>
+				array(
+					'pretty_version' => 'dev-master',
+					'version'        => 'dev-master',
+					'aliases'        =>
+						array(),
+					'reference'      => '8e9e4fbaf07a4b85a5e54e2eb4deaa2ee93ccc2b',
+				),
+			'simplehtmldom/simplehtmldom' =>
+				array(
+					'pretty_version' => 'dev-master',
+					'version'        => 'dev-master',
+					'aliases'        =>
+						array(
+							0 => '9999999-dev',
+						),
+					'reference'      => '31ce157e7ba0c6ca35f94faedf031a945c3f6427',
+				),
+		),
+);
