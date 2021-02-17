8cc6778997dad8300ba56c478b2ba4b8187ed461
Azeez <azeezs2012@gmail.com>
2021-02-17 12:39:44 +0530

    feat : add pagination order by id

--- includes/UnusedCSS_DB.php
+++ includes/UnusedCSS_DB.php
@@ -263,7 +263,7 @@ class UnusedCSS_DB
     static function get_links($start_from = 0, $limit = 10, $where = ''){
         global $wpdb;
 
-	    $links = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}rapidload_uucss_job {$where} LIMIT {$start_from},{$limit}", OBJECT);
+	    $links = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}rapidload_uucss_job {$where} ORDER BY id DESC LIMIT {$start_from},{$limit}", OBJECT);
 
 	    $links = array_map(function ($link){
 		    return self::transform_link($link);
