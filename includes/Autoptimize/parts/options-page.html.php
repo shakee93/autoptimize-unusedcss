d3f31b4862ffde1a698eef59a79cb362e418a744
Azeez <azeezs2012@gmail.com>
2021-02-22 11:19:02 +0530

    refactor : update if condition

--- includes/Autoptimize/parts/options-page.html.php
+++ includes/Autoptimize/parts/options-page.html.php
@@ -69,15 +69,9 @@
                         <p>
                             Failed Jobs : <?php echo $failed; ?> - <?php echo number_format($failed/$total*100, 2); ?>%
                         </p>
-                        <?php
-                            if(!empty($third_party_cache_plugins)) :
-                        ?>
-                                <div class="status-action-wrap">
-                                    <input class="clear-warnings-page-cache button button-primary" type="button" value="Clear Page Cache for Warnings">
-                                </div>
-                        <?
-                            endif;
-                        ?>
+                        <div class="status-action-wrap">
+                            <input class="clear-warnings-page-cache button button-primary" type="button" value="Clear Page Cache for Warnings">
+                        </div>
                     </div>
                 </li>
                 <li>
