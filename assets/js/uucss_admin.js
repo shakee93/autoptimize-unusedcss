24bdfb83913da371617f324a23da3bc8f609a129
Azeez <azeezs2012@gmail.com>
2021-03-18 09:32:37 +0530

    feat : update hits star

--- assets/js/uucss_admin.js
+++ assets/js/uucss_admin.js
@@ -555,7 +555,7 @@
                         if (rowData.status === 'success' && (!rowData.meta.warnings || !rowData.meta.warnings.length)) {
                             var hits_star = '<span class="dashicons dashicons-star-empty"></span>';
                             if(rowData.meta && rowData.meta.stats && rowData.meta.stats.success_count > 0){
-                                hits_star = '<span style="margin-left: -3px; margin-top: -3px" class="dashicons dashicons-star-filled"></span>';
+                                hits_star = '<span class="dashicons dashicons-star-filled"></span>';
                             }
                             stat.find('span').append('<span class="dashicons dashicons-yes-alt"></span>' + hits_star);
                             tippy(stat.find('span')[0], tippyOptions);
