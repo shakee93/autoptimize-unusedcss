cdda255e75c8bbe4f7f743a04d8e504c2c9d78cf
Azeez <azeezs2012@gmail.com>
2021-03-24 16:59:06 +0530

    refactor : update filters to actions

--- includes/UnusedCSS_Enqueue.php
+++ includes/UnusedCSS_Enqueue.php
@@ -9,29 +9,31 @@ class UnusedCSS_Enqueue {
     private $file_system;
 
     private $inject;
+    private $dom;
+    private $uucss_autoptimize;
+    private $data;
 
     function __construct()
     {
         $this->file_system = new UnusedCSS_FileSystem();
 
-        add_filter('uucss/enqueue/before-enqueue', [$this, 'before_enqueue'], 10, 2);
         add_filter('uucss/enqueue/inject-css', [$this, 'inject_css'], 10, 3);
-        add_filter('uucss/enqueue/replace-inline-css', [$this, 'replace_inline_css'], 10, 3);
-        add_filter('uucss/enqueue/replace-style-sheets', [$this, 'replace_style_sheets'], 10, 3);
-        add_filter('uucss/enqueue/enqueue-completed', [$this, 'enqueue_completed'], 10, 2);
+        add_action('uucss/enqueue/before-enqueue', [$this, 'before_enqueue']);
+        add_action('uucss/enqueue/replace-inline-css', [$this, 'replace_inline_css']);
+        add_action('uucss/enqueue/replace-style-sheets', [$this, 'replace_style_sheets']);
+        add_action('uucss/enqueue/enqueue-completed', [$this, 'enqueue_completed']);
     }
 
-    public function replace_inline_css($dom , $data, $autoptimize){
+    public function replace_inline_css(){
 
-        $inline_styles = $dom->find('style');
+        $inline_styles = $this->dom->find('style');
 
-        if(isset($autoptimize->options['uucss_include_inline_css']) &&
-            $autoptimize->options['uucss_include_inline_css'] == '1' &&
+        if(isset($this->uucss_autoptimize->options['uucss_include_inline_css']) &&
+            $this->uucss_autoptimize->options['uucss_include_inline_css'] == '1' &&
             autoptimizeOptionWrapper::get_option( 'autoptimize_css_include_inline' ) != 'on' &&
-            isset($data['files']) && !empty($data['files'])){
+            isset($this->data['files']) && !empty($this->data['files'])){
 
             $inline_style_content = '';
-            $warnings = [];
 
             foreach ($inline_styles as $style){
 
@@ -49,24 +51,23 @@ class UnusedCSS_Enqueue {
 
                 $search = '//inline-style@' . md5(self::remove_white_space($style->innertext));
 
-                $file_key = array_search( $search, array_column( $data['files'], 'original' ) );
+                $file_key = array_search( $search, array_column( $this->data['files'], 'original' ) );
 
                 if(is_numeric( $file_key ) && $file_key){
 
                     $style->outertext = '';
-                    $inline_style_content .= $data['files'][$file_key]['uucss'];
+                    $inline_style_content .= $this->data['files'][$file_key]['uucss'];
 
                 }else{
 
                     $this->inject->successfully_injected = false;
 
-                    if(!in_array($search, array_column($data['meta']['warnings'], 'file'))){
+                    if(!in_array($search, array_column($this->data['meta']['warnings'], 'file'))){
 
-                        $warnings[] = [
+                        $this->data['meta']['warnings'][] = [
                             "file" => $search,
                             "message" => "RapidLoad optimized version for the inline style missing."
                         ];
-
                     }
 
                 }
@@ -74,33 +75,21 @@ class UnusedCSS_Enqueue {
 
             if(!empty($inline_style_content)){
 
-                $inline_style_content = '<style inlined-uucss="uucss-inline-' . md5($data['url']) . '" uucss>' . $inline_style_content . '</style>';
+                $inline_style_content = '<style inlined-uucss="uucss-inline-' . md5($this->data['url']) . '" uucss>' . $inline_style_content . '</style>';
 
-                $header_content = $dom->find( 'head' )[0]->outertext;
+                $header_content = $this->dom->find( 'head' )[0]->outertext;
                 $header_content = str_replace('</head>','', $header_content);
 
-                $dom->find( 'head' )[0]->outertext = $header_content . $inline_style_content . '</head>';
+                $this->dom->find( 'head' )[0]->outertext = $header_content . $inline_style_content . '</head>';
 
             }
 
-            if(!empty($warnings)){
-
-                UnusedCSS_DB::update_meta([
-                    'warnings' => array_merge($data['meta']['warnings'], $warnings)
-                ], $data['url']);
-
-                self::log([
-                    'log' => 'inline style not found warnings added',
-                    'url' => $data['url'],
-                    'type' => 'injection'
-                ]);
-            }
         }
 
-        return $dom;
+        return $this->dom;
     }
 
-    public function before_enqueue($dom, $data){
+    public function before_enqueue(){
 
         $this->inject = (object) [
             "parsed_html"           => false,
@@ -114,83 +103,83 @@ class UnusedCSS_Enqueue {
 
         $this->inject->parsed_html = true;
 
-        $dom->find( 'html' )[0]->uucss = true;
+        $this->dom->find( 'html' )[0]->uucss = true;
 
         self::log([
             'log' => 'header injection done',
-            'url' => $data['url'],
+            'url' => $this->data['url'],
             'type' => 'injection'
         ]);
 
-        return $dom;
+        return $this->dom;
     }
 
-    public function enqueue_completed($dom, $data){
+    public function enqueue_completed(){
 
         $time_diff = 0;
 
-        if(isset($data['time'])){
-            $time_diff = time() - $data['time'];
+        if(isset($this->data['time'])){
+            $time_diff = time() - $this->data['time'];
         }
 
         self::log([
             'log' => json_encode((array) $this->inject),
-            'url' => $data['url'],
+            'url' => $this->data['url'],
             'type' => 'injection'
         ]);
 
 
         if($this->inject->successfully_injected){
 
-            if($data['attempts'] > 0){
+            if($this->data['attempts'] > 0){
 
-                UnusedCSS_DB::reset_attempts($data['url']);
+                UnusedCSS_DB::reset_attempts($this->data['url']);
             }
 
-            $dom->find( 'body' )[0]->uucss = true;
+            $this->dom->find( 'body' )[0]->uucss = true;
 
-            UnusedCSS_DB::update_success_count($data['url']);
+            UnusedCSS_DB::update_success_count($this->data['url']);
 
             self::log([
                 'log' => 'injection success',
-                'url' => $data['url'],
+                'url' => $this->data['url'],
                 'type' => 'injection'
             ]);
 
-        }else if(!$this->inject->successfully_injected && ($data['attempts'] <= 2 || ($time_diff > 86400))){
+        }else if(!$this->inject->successfully_injected && ($this->data['attempts'] <= 2 || ($time_diff > 86400))){
 
             UnusedCSS_DB::update_meta([
                 'status' => 'queued',
-                'attempts' => $data['attempts'] + 1,
+                'attempts' => $this->data['attempts'] + 1,
                 'created_at' => date( "Y-m-d H:m:s", time() )
-            ], $data['url']);
+            ], $this->data['url']);
 
             self::log([
                 'log' => 're-queued due to warnings',
-                'url' => $data['url'],
+                'url' => $this->data['url'],
                 'type' => 'injection'
             ]);
 
         }else{
 
             UnusedCSS_DB::update_meta([
-                'warnings' => $data['meta']['warnings']
-            ], $data['url']);
+                'warnings' => $this->data['meta']['warnings']
+            ], $this->data['url']);
 
             self::log([
                 'log' => 'file not found warnings added',
-                'url' => $data['url'],
+                'url' => $this->data['url'],
                 'type' => 'injection'
             ]);
 
         }
 
-        return $dom;
+        return $this->dom;
     }
 
-    public function replace_style_sheets($dom , $data, $autoptimize){
+    public function replace_style_sheets(){
 
-        $sheets = $dom->find( 'link' );
+        $sheets = $this->dom->find( 'link' );
 
         foreach ( $sheets as $sheet ) {
 
@@ -216,7 +205,7 @@ class UnusedCSS_Enqueue {
 
                     if(isset($url_parts['path'])){
 
-                        $result = preg_grep('~' . $url_parts['path'] . '~', array_column( $data['files'], 'original' ));
+                        $result = preg_grep('~' . $url_parts['path'] . '~', array_column( $this->data['files'], 'original' ));
 
                         $key = isset($result) && !empty($result) ? key($result) : null;
                     }
@@ -225,27 +214,27 @@ class UnusedCSS_Enqueue {
 
                     $link = apply_filters('uucss/cdn-url', $link);
 
-                    $file = array_search( $link, array_column( $data['files'], 'original' ) );
+                    $file = array_search( $link, array_column( $this->data['files'], 'original' ) );
 
                     if ( ! $file ) {
                         // Retry to see if file can be found with CDN url
-                        $file = array_search( $autoptimize->uucss_ao_base->url_replace_cdn($link), array_column( $data['files'], 'original' ) );
+                        $file = array_search( $this->uucss_autoptimize->uucss_ao_base->url_replace_cdn($link), array_column( $this->data['files'], 'original' ) );
                     }
 
-                    $key = isset($data['files']) ? $file : null;
+                    $key = isset($this->data['files']) ? $file : null;
 
                 }
 
                 // check if we found a script index and the file exists
-                if ( is_numeric( $key ) && $this->file_system->exists( UnusedCSS::$base_dir . '/' . $data['files'][ $key ]['uucss'] ) ) {
-                    $uucss_file = $data['files'][ $key ]['uucss'];
+                if ( is_numeric( $key ) && $this->file_system->exists( UnusedCSS::$base_dir . '/' . $this->data['files'][ $key ]['uucss'] ) ) {
+                    $uucss_file = $this->data['files'][ $key ]['uucss'];
 
                     array_push( $this->inject->found_css_cache_files, $link );
 
-                    $newLink = $autoptimize->get_cached_file( $uucss_file, $autoptimize->uucss_ao_base->cdn_url );
+                    $newLink = $this->uucss_autoptimize->get_cached_file( $uucss_file, $this->uucss_autoptimize->uucss_ao_base->cdn_url );
 
                     // check the file is processed via AO
-                    $is_ao_css = $autoptimize->ao_handled($link);
+                    $is_ao_css = $this->uucss_autoptimize->ao_handled($link);
 
                     if($is_ao_css){
 
@@ -259,13 +248,13 @@ class UnusedCSS_Enqueue {
 
                     }
 
-                    if ( $is_ao_css || isset( $autoptimize->options['autoptimize_uucss_include_all_files'] ) ) {
+                    if ( $is_ao_css || isset( $this->uucss_autoptimize->options['autoptimize_uucss_include_all_files'] ) ) {
 
                         $sheet->uucss = true;
                         $sheet->href  = $newLink;
 
-                        if ( isset( $autoptimize->options['uucss_inline_css'] ) ) {
-                            $autoptimize->inline_sheet( $sheet, $uucss_file );
+                        if ( isset( $this->uucss_autoptimize->options['uucss_inline_css'] ) ) {
+                            $this->uucss_autoptimize->inline_sheet( $sheet, $uucss_file );
                         }
 
                         array_push( $this->inject->injected_css_files, $newLink );
@@ -277,13 +266,13 @@ class UnusedCSS_Enqueue {
 
                     $uucss_injected = $sheet->getAttribute('uucss');
 
-                    if(!$uucss_injected && !$autoptimize->is_file_excluded($autoptimize->options, $link)){
+                    if(!$uucss_injected && !$this->uucss_autoptimize->is_file_excluded($this->uucss_autoptimize->options, $link)){
 
                         $this->inject->successfully_injected = false;
 
-                        if(!in_array($link, array_column($data['meta']['warnings'], 'file'))){
+                        if(!in_array($link, array_column($this->data['meta']['warnings'], 'file'))){
 
-                            $data['meta']['warnings'][] = [
+                            $this->data['meta']['warnings'][] = [
                                 "file" => $link,
                                 "message" => "RapidLoad optimized version for the file missing."
                             ];
@@ -297,14 +286,16 @@ class UnusedCSS_Enqueue {
 
         }
 
-        return $dom;
+        return $this->dom;
     }
 
     public function inject_css($html, $data, $autoptimize){
 
+        $this->data = $data;
+
         self::log([
             'log' => 'injection started',
-            'url' => $data['url'],
+            'url' => $this->data['url'],
             'type' => 'injection'
         ]);
 
@@ -313,7 +304,7 @@ class UnusedCSS_Enqueue {
             return $html;
         }
 
-        $dom = new \simplehtmldom\HtmlDocument(
+        $this->dom = new \simplehtmldom\HtmlDocument(
             $html,
             false,
             false,
@@ -321,19 +312,21 @@ class UnusedCSS_Enqueue {
             false
         );
 
-        if ( $dom ) {
+        $this->uucss_autoptimize = $autoptimize;
 
-            $dom = apply_filters('uucss/enqueue/before-enqueue', $dom, $data);
+        if ( $this->dom ) {
 
-            $dom = apply_filters('uucss/enqueue/replace-inline-css', $dom, $data, $autoptimize);
+            do_action('uucss/enqueue/before-enqueue');
 
-            $dom = apply_filters('uucss/enqueue/replace-style-sheets', $dom, $data, $autoptimize);
+            do_action('uucss/enqueue/replace-inline-css');
 
-            $dom = apply_filters('uucss/enqueue/enqueue-completed', $dom, $data);
+            do_action('uucss/enqueue/replace-style-sheets');
+
+            do_action('uucss/enqueue/enqueue-completed');
 
             header( 'uucss:' . 'v' . UUCSS_VERSION . ' [' . count( $this->inject->found_css_files ) . count( $this->inject->found_css_cache_files ) . count( $this->inject->injected_css_files ) . ']' );
 
-            return $dom;
+            return $this->dom;
         }
 
         return $html;
