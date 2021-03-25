d6e3d1184ca591498d80926f960ed1ed387f63b4
Azeez <azeezs2012@gmail.com>
2021-03-25 17:27:07 +0530

    refactor : refine plugin

--- includes/Autoptimize/UnusedCSS_Autoptimize_Admin.php
+++ includes/Autoptimize/UnusedCSS_Autoptimize_Admin.php
@@ -25,12 +25,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 			return;
 		}
 
-
-		add_filter( 'plugin_action_links_' . plugin_basename( UUCSS_PLUGIN_FILE ), [
-			$this,
-			'add_plugin_action_link'
-		] );
-
 		add_action( 'current_screen', function () {
 
 			if ( get_current_screen() && get_current_screen()->base == 'settings_page_uucss' ) {
@@ -54,12 +48,7 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 			add_action( "wp_ajax_uucss_deactivate", [ $this, 'ajax_deactivate' ] );
 			add_action( "wp_ajax_uucss_data", [ $this, 'uucss_data' ] );
 			add_action( "wp_ajax_uucss_connect", [ $this, 'uucss_connect' ] );
-			add_action( "wp_ajax_uucss_test_url", [ $this, 'uucss_test_url' ] );
-            add_action('wp_ajax_uucss_logs', [$this, 'uucss_logs']);
-            add_action('wp_ajax_clear_uucss_logs', [$this, 'clear_uucss_logs']);
-            add_action('wp_ajax_frontend_logs', [$this, 'frontend_logs']);
-            add_action('wp_ajax_clear_page_cache', [$this, 'clear_page_cache']);
-            add_action('wp_ajax_mark_faqs_read', [$this, 'mark_faqs_read']);
+			add_filter('uucss/notifications', [$this, 'addNotifications'], 10, 1);
 
 			add_action( 'admin_notices', [ $this, 'first_uucss_job' ] );
 
@@ -93,142 +82,7 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 
     }
 
-    function mark_faqs_read(){
-
-        self::update_site_option('rapidload_faqs_read', true);
-        wp_send_json_success(true);
-    }
-
-    function clear_page_cache(){
-
-	    $url = isset($_REQUEST['url']) ? $_REQUEST['url'] : false;
-
-	    $status = isset($_REQUEST['status']) ? $_REQUEST['status'] : false;
-
-        if($url){
-
-            do_action( 'uucss/cached', [
-                'url' => $url
-            ] );
-        }
-
-        $links = false;
-
-        if($status && $status == 'warnings'){
-
-            $links = UnusedCSS_DB::get_links_where(' WHERE warnings IS NOT NULL ');
-
-        }
-
-        if($links && !empty($links)){
-
-            foreach ($links as $link){
-
-                if(isset($link['url'])){
-
-                    do_action( 'uucss/cached', [
-                        'url' => $link['url']
-                    ] );
-                }
-            }
-        }
-
-	    wp_send_json_success('page cache cleared');
-    }
-
-    function frontend_logs(){
-
-	    $args = [];
-
-	    $args['type'] = isset($_REQUEST['type']) && !empty($_REQUEST['type']) ? $_REQUEST['type'] : 'frontend';
-	    $args['log'] = isset($_REQUEST['log']) && !empty($_REQUEST['log']) ? $_REQUEST['log'] : '';
-	    $args['url'] = isset($_REQUEST['url']) && !empty($_REQUEST['url']) ? $_REQUEST['url'] : '';
-
-	    self::log($args);
-
-	    wp_send_json_success(true);
-    }
-
-    function uucss_logs(){
-
-	    $file_system = new UnusedCSS_FileSystem();
-
-	    if(!$file_system->exists(UUCSS_LOG_DIR . 'debug.log')){
-            wp_send_json_success([]);
-        }
-
-	    $data = $file_system->get_contents(UUCSS_LOG_DIR . 'debug.log');
-
-	    if(empty($data)){
-	        wp_send_json_success([]);
-        }
-
-	    $data = '[' . $data . ']';
-
-        wp_send_json_success(json_decode($data));
-    }
-
-    function clear_uucss_logs(){
-        $file_system = new UnusedCSS_FileSystem();
-
-        if(!$file_system->exists(WP_CONTENT_DIR . '/uploads/rapidload/')){
-            wp_send_json_success(true);
-        }
-
-        $file_system->delete_folder(WP_CONTENT_DIR . '/uploads/rapidload/');
-        wp_send_json_success(true);
-    }
-
-    public function get_faqs(){
-
-        $rapidload_faqs_read = self::get_site_option('rapidload_faqs_read');
-
-        if(!empty($rapidload_faqs_read)){
-            return [];
-        }
-
-        $api = new UnusedCSS_Api();
-
-        $result = $api->get('faqs');
-
-        $default = [
-	        [
-		        "title" => "I enabled RapidLoad and now my site is broken. What do I do?",
-		        "message" => "If you are encountering layout or styling issues on a RapidLoad optimized page, try enabling the “Load Original CSS Files” option or <a href='https://rapidload.zendesk.com/hc/en-us/articles/360063292673-Sitewide-Safelists-Blocklists'>adding safelist rules</a> for affected elements in the plugin Advanced Settings. Always remember to requeue affected pages after making plugin changes. Need more help? Head over to the RapidLoad docs for more information or to submit a Support request: <a href='https://rapidload.zendesk.com/hc/en-us'>https://rapidload.zendesk.com/hc/en-us</a>",
-	        ],
-	        [
-		        "title" => "Why am I still seeing the “Removed unused CSS” flag in Google Page Speed Insights?",
-		        "message" => "It’s possible that the RapidLoad optimized version of the page is not yet being served. Try clearing your page cache and running the GPSI test again.",
-	        ],
-	        [
-		        "title" => "Will this plugin work with other caching plugins?",
-		        "message" => "RapidLoad works with all major caching plugins. If you are using a little known caching plugin and are experiencing issues with RapidLoad, please submit your issue and caching plugin name to our support team and we will review.",
-	        ],
-	        [
-		        "title" => "Do I need to run this every time I make a change?",
-		        "message" => "No! RapidLoad works in the background, so any new stylesheets that are added will be analyzed and optimized on the fly. Just set it and forget it!",
-	        ],
-	        [
-		        "title" => "Do you offer support if I need it?",
-		        "message" => "Yes, our team is standing by to assist you! Submit a support ticket any time from the Support tab in the plugin and we’ll be happy to help.",
-	        ]
-        ];
-
-        return !$api->is_error($result) && isset($result->data) ? $result->data : $default;
-    }
-
-    public function get_public_notices(){
-
-	    return [];
-        /*$api = new UnusedCSS_Api();
-
-        $result = $api->get('notification');
-
-        return !$api->is_error($result) && isset($result->data) ? $result->data : [];*/
-    }
-
-	public function getNotifications() {
-		$notifications = [];
+	public function addNotifications($notifications) {
 
 		if (!(bool) autoptimizeOptionWrapper::get_option( 'autoptimize_cache_nogzip' )) {
 		    $notifications[] = [
@@ -279,8 +133,8 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 			'url' => site_url(),
 			'ajax_url'          => admin_url( 'admin-ajax.php' ),
 			'setting_url'       => admin_url( 'options-general.php?page=uucss' ),
-			'on_board_complete' => UnusedCSS_Autoptimize_Onboard::on_board_completed(),
-			'api_key_verified' => UnusedCSS_Autoptimize_Admin::is_api_key_verified(),
+			'on_board_complete' => apply_filters('uucss/on-board/complete', false),
+			'api_key_verified' => self::is_api_key_verified(),
             'notifications' => $this->getNotifications(),
             'faqs' => $this->get_faqs(),
             'public_notices' => $this->get_public_notices()
@@ -303,60 +157,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 
         return array_merge( $_links, $links );
     }
-
-	public function add_plugin_action_link( $links ) {
-
-		$_links = array(
-			'<a href="' . admin_url( 'options-general.php?page=uucss' ) . '">Settings</a>',
-		);
-
-		return array_merge( $_links, $links );
-	}
-
-	function uucss_test_url(){
-
-	    if(!isset($_REQUEST['url'])){
-	        wp_send_json_error('url required');
-        }
-
-	    $url = $_REQUEST['url'];
-
-        $uucss_api = new UnusedCSS_Api();
-
-        $link = UnusedCSS_DB::get_link($url);
-
-        $cached_files = [];
-        $original_files = [];
-
-        if(isset($link['files']) && !empty($link['files'])){
-
-            $cached_files = array_filter($link['files'], function ($file){
-               return !$this->str_contains($file['original'], '//inline-style@');
-            });
-
-            $original_files = array_filter($link['files'], function ($file){
-                return !$this->str_contains($file['original'], '//inline-style@');
-            });
-        }
-
-        $result = $uucss_api->post( 'test/wordpress',
-            [
-                'url' => urldecode($url),
-                'files' => !empty($cached_files) ? array_column($cached_files, 'uucss') : [],
-                'aoFiles' => !empty($original_files) ? array_column($original_files, 'original') : []
-            ]);
-
-        if ( $uucss_api->is_error( $result ) ) {
-            if(isset($result->errors) && isset($result->errors[0])){
-                wp_send_json_error($result->errors[0]->detail);
-            }else{
-                wp_send_json_error($result);
-            }
-        }
-
-        wp_send_json_success($result);
-    }
-
 
 	function first_uucss_job() {
 
@@ -545,15 +345,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
         return apply_filters('uucss/enabled', true);
     }
 
-
-	public static function is_api_key_verified() {
-
-		$api_key_status = isset( static::fetch_options()['uucss_api_key_verified'] ) ? static::fetch_options()['uucss_api_key_verified'] : '';
-
-		return $api_key_status == '1';
-
-	}
-
 	public function validate_domain() {
 
 		if ( get_current_screen() && get_current_screen()->base != 'settings_page_uucss' ) {
@@ -591,7 +382,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
         return  $options['valid_domain'];
     }
 
-
 	public function add_ao_tab( $in ) {
 
 		$tab = 'RapidLoad';
@@ -602,7 +392,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 
 		return $in;
 	}
-
 
     public function add_ao_page() {
 
@@ -625,16 +414,12 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 
     }
 
-
 	public function render_form() {
 		$options = $this->fetch_options();
 		include( 'parts/options-page.html.php' );
 	}
 
-
-
 	public function deactivate() {
-
 
 		if ( ! isset( $_REQUEST['deactivated'] ) || empty( $_REQUEST['deactivated'] ) ) {
 			return;
@@ -675,7 +460,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 		return;
 	}
 
-
 	public function ajax_deactivate() {
 
 		$options = self::get_site_option( 'autoptimize_uucss_settings' );
@@ -700,7 +484,6 @@ class UnusedCSS_Autoptimize_Admin extends UnusedCSS_Admin {
 
 		wp_send_json_success( true );
 	}
-
 
 	public function clear_cache_on_option_update( $option, $old_value, $value ) {
 
--- includes/UnusedCSS_Admin.php
+++ includes/UnusedCSS_Admin.php
@@ -47,9 +47,219 @@ abstract class UnusedCSS_Admin {
 	    add_action( 'add_meta_boxes', [$this, 'add_meta_boxes'] );
 	    add_action( 'save_post', [$this, 'save_meta_box_options'] , 10, 2);
 
+        add_action('wp_ajax_clear_page_cache', [$this, 'clear_page_cache']);
+        add_action('wp_ajax_mark_faqs_read', [$this, 'mark_faqs_read']);
+        add_action('wp_ajax_frontend_logs', [$this, 'frontend_logs']);
+        add_action('wp_ajax_uucss_logs', [$this, 'uucss_logs']);
+        add_action('wp_ajax_clear_uucss_logs', [$this, 'clear_uucss_logs']);
+        add_action( "wp_ajax_uucss_test_url", [ $this, 'uucss_test_url' ] );
+
+        add_filter( 'plugin_action_links_' . plugin_basename( UUCSS_PLUGIN_FILE ), [
+            $this,
+            'add_plugin_action_link'
+        ] );
+
     }
 
+    public function getNotifications() {
 
+        return apply_filters('uucss/notifications', []);
+    }
+
+    public function get_public_notices(){
+
+        return [];
+        /*$api = new UnusedCSS_Api();
+
+        $result = $api->get('notification');
+
+        return !$api->is_error($result) && isset($result->data) ? $result->data : [];*/
+    }
+
+    function uucss_test_url(){
+
+        if(!isset($_REQUEST['url'])){
+            wp_send_json_error('url required');
+        }
+
+        $url = $_REQUEST['url'];
+
+        $uucss_api = new UnusedCSS_Api();
+
+        $link = UnusedCSS_DB::get_link($url);
+
+        $cached_files = [];
+        $original_files = [];
+
+        if(isset($link['files']) && !empty($link['files'])){
+
+            $cached_files = array_filter($link['files'], function ($file){
+                return !$this->str_contains($file['original'], '//inline-style@');
+            });
+
+            $original_files = array_filter($link['files'], function ($file){
+                return !$this->str_contains($file['original'], '//inline-style@');
+            });
+        }
+
+        $result = $uucss_api->post( 'test/wordpress',
+            [
+                'url' => urldecode($url),
+                'files' => !empty($cached_files) ? array_column($cached_files, 'uucss') : [],
+                'aoFiles' => !empty($original_files) ? array_column($original_files, 'original') : []
+            ]);
+
+        if ( $uucss_api->is_error( $result ) ) {
+            if(isset($result->errors) && isset($result->errors[0])){
+                wp_send_json_error($result->errors[0]->detail);
+            }else{
+                wp_send_json_error($result);
+            }
+        }
+
+        wp_send_json_success($result);
+    }
+
+    public function get_faqs(){
+
+        $rapidload_faqs_read = self::get_site_option('rapidload_faqs_read');
+
+        if(!empty($rapidload_faqs_read)){
+            return [];
+        }
+
+        $api = new UnusedCSS_Api();
+
+        $result = $api->get('faqs');
+
+        $default = [
+            [
+                "title" => "I enabled RapidLoad and now my site is broken. What do I do?",
+                "message" => "If you are encountering layout or styling issues on a RapidLoad optimized page, try enabling the “Load Original CSS Files” option or <a href='https://rapidload.zendesk.com/hc/en-us/articles/360063292673-Sitewide-Safelists-Blocklists'>adding safelist rules</a> for affected elements in the plugin Advanced Settings. Always remember to requeue affected pages after making plugin changes. Need more help? Head over to the RapidLoad docs for more information or to submit a Support request: <a href='https://rapidload.zendesk.com/hc/en-us'>https://rapidload.zendesk.com/hc/en-us</a>",
+            ],
+            [
+                "title" => "Why am I still seeing the “Removed unused CSS” flag in Google Page Speed Insights?",
+                "message" => "It’s possible that the RapidLoad optimized version of the page is not yet being served. Try clearing your page cache and running the GPSI test again.",
+            ],
+            [
+                "title" => "Will this plugin work with other caching plugins?",
+                "message" => "RapidLoad works with all major caching plugins. If you are using a little known caching plugin and are experiencing issues with RapidLoad, please submit your issue and caching plugin name to our support team and we will review.",
+            ],
+            [
+                "title" => "Do I need to run this every time I make a change?",
+                "message" => "No! RapidLoad works in the background, so any new stylesheets that are added will be analyzed and optimized on the fly. Just set it and forget it!",
+            ],
+            [
+                "title" => "Do you offer support if I need it?",
+                "message" => "Yes, our team is standing by to assist you! Submit a support ticket any time from the Support tab in the plugin and we’ll be happy to help.",
+            ]
+        ];
+
+        return !$api->is_error($result) && isset($result->data) ? $result->data : $default;
+    }
+
+    function clear_uucss_logs(){
+        $file_system = new UnusedCSS_FileSystem();
+
+        if(!$file_system->exists(WP_CONTENT_DIR . '/uploads/rapidload/')){
+            wp_send_json_success(true);
+        }
+
+        $file_system->delete_folder(WP_CONTENT_DIR . '/uploads/rapidload/');
+        wp_send_json_success(true);
+    }
+
+    function uucss_logs(){
+
+        $file_system = new UnusedCSS_FileSystem();
+
+        if(!$file_system->exists(UUCSS_LOG_DIR . 'debug.log')){
+            wp_send_json_success([]);
+        }
+
+        $data = $file_system->get_contents(UUCSS_LOG_DIR . 'debug.log');
+
+        if(empty($data)){
+            wp_send_json_success([]);
+        }
+
+        $data = '[' . $data . ']';
+
+        wp_send_json_success(json_decode($data));
+    }
+
+    function frontend_logs(){
+
+        $args = [];
+
+        $args['type'] = isset($_REQUEST['type']) && !empty($_REQUEST['type']) ? $_REQUEST['type'] : 'frontend';
+        $args['log'] = isset($_REQUEST['log']) && !empty($_REQUEST['log']) ? $_REQUEST['log'] : '';
+        $args['url'] = isset($_REQUEST['url']) && !empty($_REQUEST['url']) ? $_REQUEST['url'] : '';
+
+        self::log($args);
+
+        wp_send_json_success(true);
+    }
+
+    function mark_faqs_read(){
+
+        self::update_site_option('rapidload_faqs_read', true);
+        wp_send_json_success(true);
+    }
+
+    function clear_page_cache(){
+
+        $url = isset($_REQUEST['url']) ? $_REQUEST['url'] : false;
+
+        $status = isset($_REQUEST['status']) ? $_REQUEST['status'] : false;
+
+        if($url){
+
+            do_action( 'uucss/cached', [
+                'url' => $url
+            ] );
+        }
+
+        $links = false;
+
+        if($status && $status == 'warnings'){
+
+            $links = UnusedCSS_DB::get_links_where(' WHERE warnings IS NOT NULL ');
+
+        }
+
+        if($links && !empty($links)){
+
+            foreach ($links as $link){
+
+                if(isset($link['url'])){
+
+                    do_action( 'uucss/cached', [
+                        'url' => $link['url']
+                    ] );
+                }
+            }
+        }
+
+        wp_send_json_success('page cache cleared');
+    }
+
+    public static function is_api_key_verified() {
+
+        $api_key_status = isset( static::fetch_options()['uucss_api_key_verified'] ) ? static::fetch_options()['uucss_api_key_verified'] : '';
+
+        return $api_key_status == '1';
+
+    }
+
+    public function add_plugin_action_link( $links ) {
+
+        $_links = array(
+            '<a href="' . admin_url( 'options-general.php?page=uucss' ) . '">Settings</a>',
+        );
+
+        return array_merge( $_links, $links );
+    }
 
     public function add_meta_boxes()
     {
