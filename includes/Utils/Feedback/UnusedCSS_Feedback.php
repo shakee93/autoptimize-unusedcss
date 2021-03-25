ececd33df90632fe66a1b8b2a5d3cf969035d504
Azeez <azeezs2012@gmail.com>
2021-03-25 17:00:57 +0530

    refactor : refine plugin

--- includes/Utils/Feedback/UnusedCSS_Feedback.php
+++ includes/Utils/Feedback/UnusedCSS_Feedback.php
@@ -21,11 +21,11 @@ class UnusedCSS_Feedback {
 
 	function enqueue_feedback_scripts(){
 
-		wp_enqueue_script( 'uucss_feedback', UUCSS_PLUGIN_URL . 'assets/js/utils/uucss_feedback.js?v=1.0', array(
+		wp_enqueue_script( 'uucss_feedback', UUCSS_PLUGIN_URL . 'assets/js/utils/uucss_feedback.js', array(
 			'jquery'
-		) );
+		),UUCSS_VERSION );
 
-		wp_enqueue_style('uucss_feedback', UUCSS_PLUGIN_URL . 'assets/css/utils/uucss_feedback.css?v=1.0');
+		wp_enqueue_style('uucss_feedback', UUCSS_PLUGIN_URL . 'assets/css/utils/uucss_feedback.css', null, UUCSS_VERSION);
 	}
 
 	function render_feedback_model(){
