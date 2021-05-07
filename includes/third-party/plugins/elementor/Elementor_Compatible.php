3501308d63c25b9e5979c0fc5b62a23d1302803a
Azeez <azeezs2012@gmail.com>
2021-05-07 11:35:57 +0530

    refactor : add elementor support

---
+++ includes/third-party/plugins/elementor/Elementor_Compatible.php
@@ -0,0 +1,36 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+class Elementor_Compatible extends RapidLoad_ThirdParty{
+
+    function __construct(){
+
+        $this->plugin = 'elementor/elementor.php';
+        $this->catgeory = 'theme-builder';
+        $this->name = 'elementor';
+
+        parent::__construct();
+    }
+
+    public function init_hooks()
+    {
+        add_filter('uucss/url/exclude', [$this, 'handle']);
+    }
+
+    public function handle($args)
+    {
+        $url_parts = parse_url( $args );
+
+        if(isset($url_parts['query']) && $this->str_contains($url_parts['query'], 'elementor-preview')){
+            return false;
+        }
+
+        return $args;
+    }
+
+    public function is_mu_plugin()
+    {
+        return false;
+    }
+}
