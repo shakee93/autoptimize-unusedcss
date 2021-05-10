f410f2b1fa0a4db2675d7f5b8280cb6be4ca7776
Azeez <azeezs2012@gmail.com>
2021-05-10 08:57:47 +0530

    refactor : versioning

--- assets/js/uucss_admin.js
+++ assets/js/uucss_admin.js
@@ -221,7 +221,7 @@
             });
         });*/
 
-        if(window.uucss.rules_enabled !== ""){
+        if(window.uucss.rules_enabled === ""){
             $('#uucss-wrapper li:not(:nth-child(2)) h2').click(function () {
                 $(this).parent().find('.content').slideToggle('fast');
                 $(this).find('.uucss-toggle-section').toggleClass('rotate')
