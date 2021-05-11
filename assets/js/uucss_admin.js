dfc58058bae7a14e0dd58a8f7e8737431b1895b0
Azeez <azeezs2012@gmail.com>
2021-05-11 14:37:27 +0530

    refactor : improve rule based

--- assets/js/uucss_admin.js
+++ assets/js/uucss_admin.js
@@ -1703,10 +1703,6 @@
                 $content.find('ul').append('<li class="simple-menu" data-action_name="remove_all"><a data-action_name="remove_all" href="#">Remove All</a></li>');
                 $content.find('ul').append('<li class="multi-select-menu" data-action_name="remove_selected"><a data-action_name="remove_selected" href="#">Remove Selected</a></li>');
 
-                if(window.uucss && window.uucss.dev_mode === "1"){
-                    $content.find('ul').append('<li data-action_name="run_gpsi_test"><a data-action_name="run_gpsi_test" href="#">Run GPSI Test</a></li>');
-                }
-
                 if($('#thirtd_part_cache_plugins').val() === "1"){
                     $content.find('ul').append('<li data-action_name="clear_warnings_cache"><a data-action_name="clear_warnings_cache" href="#">Clear Page Cache</a></li>');
                 }
