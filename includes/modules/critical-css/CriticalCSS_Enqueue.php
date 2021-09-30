465cf0d2c00ea83c4c481c282d498b4ff1fe1aec
azeezs2012 <azeezs2012@gmail.com>
2021-09-30 12:31:02 +0530

    fix font issue

--- includes/modules/critical-css/CriticalCSS_Enqueue.php
+++ includes/modules/critical-css/CriticalCSS_Enqueue.php
@@ -123,7 +123,7 @@ class CriticalCSS_Enqueue
             $outer_text = $sheet->outertext;
             $sheet->onload = 'this.onload=null;this.media="' . $sheet->media . '";this.rel="stylesheet";';
             $sheet->media = 'print';
-            $sheet->rel = 'preload';
+            //$sheet->rel = 'preload';
             $sheet->outertext = '<noscript>' . $outer_text . '</noscript>' . $sheet->outertext;
 
         }
