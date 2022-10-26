314176ae28d1488e0096f28db0d7ec34134c00fb
shakee93 <shakee.zats@gmail.com>
2022-10-26 18:16:03 +0530

    fix: dynamic image for dashboard feat: clean-up the dashboard

--- includes/admin/frontend/src/views/dashboard.vue
+++ includes/admin/frontend/src/views/dashboard.vue
@@ -7,18 +7,16 @@
 
     <ul class="nav-items mt-8 flex flex-direction-column gap-8">
 
-      <li v-for="item in items" :key="item.link" class="w-72 h-56 rounded-xl border border-slate-200 bg-gradient-to-l from-slate-50 ">
+      <li v-for="item in items" :key="item.link" class="w-72 h-56 drop-shadow-sm rounded-xl border border-slate-300 bg-slate-100 ">
 
         <div class="content p-4">
-          <img v-if="item.image" src="@/assets/unused-css.svg" :alt="item.name">
+          <img v-if="item.image" :src="base + item.image" :alt="item.name">
           <h4 class="mt-2 text-gray-800 font-bold">{{ item.name }}</h4>
           <p class="mb-1 text-sm text-gray-500 ">{{ item.description }}</p>
         </div>
-
-        <hr>
-
-        <div class="actions p-4">
-          <RouterLink class="rounded border border-slate-300" :to="item.link">
+        <hr class="border-slate-300">
+        <div class="actions p-4 mt-1">
+          <RouterLink class="rounded-lg hover:bg-slate-200 border border-slate-300 py-2 px-4 text-sm" :to="item.link">
             <button>Settings</button>
           </RouterLink>
         </div>
@@ -30,6 +28,8 @@
 
 <script>
 
+import config from "../config";
+
 export default {
   data() {
     return {
@@ -37,16 +37,17 @@ export default {
         {
           name: "Unused CSS",
           description : 'Reduce your CSS file size by remove unused CSS from your pages',
-          image : '@/assets/unused-css.svg',
-
+          image : 'unused-css.svg',
           link: '/unused-css'
         },
         {
           name: "Critical CSS",
+          description : 'Generate above the fold critical CSS for your pages',
+          image : 'critical-css.svg',
           link: '/critical-css'
         },
-
-      ]
+      ],
+      base: config.is_plugin ? config.public_base + '/public/images/' : 'images/'
     }
   }
 }
