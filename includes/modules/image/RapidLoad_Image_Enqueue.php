d9fb5ce101557094676a8e2eedc5cca1f29e7254
azeezs2012 <azeezs2012@gmail.com>
2022-12-01 13:49:08 +0530

    update image optimization

--- includes/modules/image/RapidLoad_Image_Enqueue.php
+++ includes/modules/image/RapidLoad_Image_Enqueue.php
@@ -18,7 +18,7 @@ class RapidLoad_Image_Enqueue
     {
         $this->job = $job;
         $this->file_system = new RapidLoad_FileSystem();
-        $this->imgExt = ["jpg", "jpeg", "png"];
+        $this->imgExt = ["jpg", "jpeg", "png", "svg"];
         $this->cdn = 'https://cdn.shortpixel.ai/spai/';
 
         add_filter('uucss/enqueue/content/update', [$this, 'update_content'], 40);
