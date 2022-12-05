ee5b9ce14b161808bf22319d434a3433fcd5e96a
azeezs2012 <azeezs2012@gmail.com>
2022-12-05 12:59:04 +0530

    update cdn purge cache function

--- includes/modules/cdn/RapidLoad_CDN.php
+++ includes/modules/cdn/RapidLoad_CDN.php
@@ -15,6 +15,19 @@ class RapidLoad_CDN
         }
 
         add_filter('uucss/enqueue/cache-file-url/cdn', [$this, 'replace_cdn'], 30);
+
+        add_action('rapidload/vanish', [ $this, 'vanish' ]);
+    }
+
+    public function vanish(){
+
+        $api = new RapidLoad_Api();
+
+        if($this->options['uucss_cdn_zone_id'] && !empty($this->options['uucss_cdn_zone_id'])){
+            $result = $api->post('purge-cdn/' . $this->options['uucss_cdn_zone_id']);
+            error_log(json_encode($result) . 'cdn purged');
+        }
+
     }
 
     public function replace_cdn($url){
