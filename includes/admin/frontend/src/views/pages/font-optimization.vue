25a5c71b094d3ca787425857383602f94c5ae95a
Shakeel <shakeel@freshpixl.com>
2022-12-06 11:42:22 +0530

    rapidload font optimization

--- includes/admin/frontend/src/views/pages/font-optimization.vue
+++ includes/admin/frontend/src/views/pages/font-optimization.vue
@@ -23,7 +23,7 @@
           <div class="grid mb-5">
             <h1 class="font-semibold text-base text-black-font">Preload Font URLs</h1>
             <p class="text-sm pb-3 text-gray-font">These selectors will be forcefully excluded from optimization.</p>
-            <textarea v-model="uucss_excluded_links"
+            <textarea v-model="preload_font_urls"
                       class="resize-none z-50 appearance-none border border-gray-button-border rounded-lg w-full py-2 px-3 h-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                       id="force-include" type="text" placeholder=""></textarea>
             <div class="-mt-3 bg-gray-lite-background rounded-lg px-4 py-4 pb-2" role="alert">
@@ -37,7 +37,7 @@
                 <div class="pr-1">
                   <div class="flex items-center mr-4 mt-3">
                     <label>
-                      <input v-model="optimize_google_fonts" type="checkbox" value=""
+                      <input v-model="self_host_google_font" type="checkbox" value=""
                              class="accent-purple w-4 h-4 transition duration-200 text-purple-600 bg-purple-100 rounded border-purple-300 dark:ring-offset-purple-800 dark:bg-purple-700 dark:border-purple-600">
                     </label>
                   </div>
@@ -55,7 +55,7 @@
                 <div class="pr-1">
                   <div class="flex items-center mr-4 mt-3">
                     <label>
-                      <input v-model="priority_fonts" type="checkbox" value=""
+                      <input v-model="display_swap_fonts" type="checkbox" value=""
                              class="accent-purple w-4 h-4 transition duration-200 text-purple-600 bg-purple-100 rounded border-purple-300 dark:ring-offset-purple-800 dark:bg-purple-700 dark:border-purple-600">
                     </label>
                   </div>
@@ -73,7 +73,7 @@
 
 
 
-          <button
+          <button @click="saveSettings"
               class="bg-transparent mb-3 text-black-font transition duration-300 hover:bg-purple font-semibold hover:text-white py-2 px-4 border border-gray-button-border hover:border-transparent mt-5 rounded-lg">
             Save Settings
           </button>
@@ -90,6 +90,7 @@
 <script>
 import config from "../../config";
 import Vue3TagsInput from 'vue3-tags-input';
+import axios from "axios";
 
 export default {
   name: "font-optimization",
@@ -98,22 +99,78 @@ export default {
     Vue3TagsInput,
   },
 
-  methods:{
+  mounted() {
 
+    const activeModules = [];
+    Object.keys(window.uucss_global.active_modules).forEach((a) => {
+      activeModules.push(window.uucss_global.active_modules[a])
+    });
+    this.font_config = activeModules;
+      console.log(this.font_config);
+    // if (this.font_config) {
+    //   Object.keys(this.font_config).map((key) => {
+    //     if (this.id === this.font_config[key].id) {
+    //       const option = this.font_config[key].options;
+    //
+    //       this.queue_option.uucss_disable_add_to_queue = option.uucss_disable_add_to_queue;
+    //       this.queue_option.uucss_disable_add_to_re_queue = option.uucss_disable_add_to_re_queue;
+    //       this.uucss_enable_debug = option.uucss_enable_debug;
+    //       this.uucss_excluded_links = option.uucss_excluded_links.replace(/,/g, '\n');
+    //       this.uucss_query_string = option.uucss_query_string;
+    //       this.queue_option.uucss_jobs_per_queue = option.uucss_jobs_per_queue < 2 ? option.uucss_jobs_per_queue + " Job" : option.uucss_jobs_per_queue + " Jobs";
+    //       this.queue_option.uucss_queue_interval = option.uucss_queue_interval > 5999 ? option.uucss_queue_interval / 6000 + " Hour" : option.uucss_queue_interval < 61 ? option.uucss_queue_interval / 60 + " Minute" : option.uucss_queue_interval / 60 + " Minutes";
+    //
+    //
+    //     }
+    //
+    //   });
+    // }
+  },
+
+  methods:{
+    saveSettings(){
+
+      const data = {
+        uucss_preload_font_urls : this.preload_font_urls.replace(/\n/g, ","),
+        uucss_self_host_google_fonts : this.self_host_google_font,
+        uucss_display_swap_fonts : this.display_swap_fonts,
+      }
+      axios.post(window.uucss_global.ajax_url + '?action=update_rapidload_settings' , data,{
+        headers: {
+          'Content-Type':'multipart/form-data'
+        }
+      })
+          .then(response => {
+            response.data;
+            window.uucss_global.active_modules = response.data.data;
+            this.$notify(
+                {
+                  group: "success",
+                  title: "Success",
+                  text: "Font Settings Updated!"
+                },
+                4000
+            );
+          } )
+          .catch(error => {
+            this.errorMessage = error.message;
+            console.error("There was an error!", error);
+          });
+
+    }
   },
 
   data() {
     return {
       base: config.is_plugin ? config.public_base + '/public/images/' : 'images/',
 
-      optimize_google_fonts: false,
-      priority_fonts: false,
+      font_config:[],
+      id: 'font',
+
+      self_host_google_font: false,
+      display_swap_fonts: false,
+      preload_font_urls: [],
       back: '/',
-      buttons: [
-        {
-          load_original_css: 'user_interaction',
-        }
-      ],
 
 
     }
