78dac31fea94910e11fad79690bf44e71d431455
azeezs2012 <azeezs2012@gmail.com>
2022-12-12 15:23:06 +0530

    update image replace function

--- assets/js/rapidload_io.js
+++ assets/js/rapidload_io.js
@@ -2,28 +2,34 @@
 
     $(document).ready(function (){
 
-        $('img').each(function (index, value){
+        window.rapidload_replace_image_src = function(){
 
-            var url = $(value).data('original-src')
+            $('img').each(function (index, value){
 
-            if(window.rapidload_io_data && url){
+                var url = $(value).data('original-src')
 
-                var options = 'ret_img'
+                if(window.rapidload_io_data && url){
 
-                if(window.rapidload_io_data.optimize_level){
-                    options += ",q_" + window.rapidload_io_data.optimize_level
+                    var options = 'ret_img'
+
+                    if(window.rapidload_io_data.optimize_level){
+                        options += ",q_" + window.rapidload_io_data.optimize_level
+                    }
+
+                    if(window.rapidload_io_data.support_next_gen_format){
+                        options += ",to_auto"
+                    }
+
+                    options += ',w_' + $(value).width()
+
+                    $(value).attr('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
                 }
 
-                if(window.rapidload_io_data.support_next_gen_format){
-                    options += ",to_auto"
-                }
+            })
 
-                options += ',w_' + $(value).width()
+        }
 
-                $(value).attr('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
-            }
-
-        })
+        window.rapidload_replace_image_src();
 
     })
 
