0a2e23bbce694d9b61524c1625a884e08fef530f
azeezs2012 <azeezs2012@gmail.com>
2022-12-15 14:21:27 +0530

    update io js

--- assets/js/rapidload_io.js
+++ assets/js/rapidload_io.js
@@ -25,34 +25,34 @@
 
         const targetNode = document.getElementsByTagName('body')[0];
 
-        const config = { attributes: true, childList: true, subtree: true };
+        const config = { attributes: false, childList: true, subtree: true };
 
         const callback = (mutationList, observer) => {
             for (const mutation of mutationList) {
                 if (mutation.type === 'childList') {
+                    console.log(mutation.addedNodes)
                     for (const node of mutation.addedNodes){
-                        if(node.toString().startsWith('<') && node.toString().endsWith('>')){
-                            try {
-                                var imageTags = node.getElementsByTagName('img');
-                                if(imageTags.length){
-                                    for (const img of imageTags){
-                                        var url = img.getAttribute('data-original-src')
-                                        if(window.rapidload_io_data && url){
-                                            var options = 'ret_img'
-                                            if(window.rapidload_io_data.optimize_level){
-                                                options += ",q_" + window.rapidload_io_data.optimize_level
-                                            }
-                                            if(window.rapidload_io_data.support_next_gen_format){
-                                                options += ",to_auto"
-                                            }
-                                            options += ',w_' + img.getBoundingClientRect().width
-                                            img.setAttribute('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
+                        console.log(node)
+                        try {
+                            var imageTags = node.getElementsByTagName('img');
+                            if(imageTags.length){
+                                for (const img of imageTags){
+                                    var url = img.getAttribute('data-original-src')
+                                    if(window.rapidload_io_data && url){
+                                        var options = 'ret_img'
+                                        if(window.rapidload_io_data.optimize_level){
+                                            options += ",q_" + window.rapidload_io_data.optimize_level
                                         }
+                                        if(window.rapidload_io_data.support_next_gen_format){
+                                            options += ",to_auto"
+                                        }
+                                        options += ',w_' + img.getBoundingClientRect().width
+                                        img.setAttribute('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
                                     }
                                 }
-                            }catch (e){
-
                             }
+                        }catch (e){
+
                         }
                     }
                 }
