2d6d20b0a58ec7d0eb417eff3c7922248a0bd4c8
azeezs2012 <azeezs2012@gmail.com>
2022-12-15 13:51:33 +0530

    update io js

--- assets/js/rapidload_io.js
+++ assets/js/rapidload_io.js
@@ -38,28 +38,30 @@
 
         const callback = (mutationList, observer) => {
             for (const mutation of mutationList) {
-                for (const node of mutation.addedNodes){
-                    var imageTags = node.getElementsByTagName('img');
-                    if(imageTags.length){
-                        for (const img of imageTags){
-                            var url = img.getAttribute('data-original-src')
+                if (mutation.type === 'childList') {
+                    for (const node of mutation.addedNodes){
+                        var imageTags = node.getElementsByTagName('img');
+                        if(imageTags.length){
+                            for (const img of imageTags){
+                                var url = img.getAttribute('data-original-src')
 
-                            if(window.rapidload_io_data && url){
+                                if(window.rapidload_io_data && url){
 
-                                var options = 'ret_img'
+                                    var options = 'ret_img'
 
-                                if(window.rapidload_io_data.optimize_level){
-                                    options += ",q_" + window.rapidload_io_data.optimize_level
+                                    if(window.rapidload_io_data.optimize_level){
+                                        options += ",q_" + window.rapidload_io_data.optimize_level
+                                    }
+
+                                    if(window.rapidload_io_data.support_next_gen_format){
+                                        options += ",to_auto"
+                                    }
+
+                                    options += ',w_' + img.getBoundingClientRect().width
+
+                                    img.setAttribute('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
+
                                 }
-
-                                if(window.rapidload_io_data.support_next_gen_format){
-                                    options += ",to_auto"
-                                }
-
-                                options += ',w_' + img.getBoundingClientRect().width
-
-                                img.setAttribute('src', window.rapidload_io_data.image_endpoint + options + '/' + url);
-
                             }
                         }
                     }
