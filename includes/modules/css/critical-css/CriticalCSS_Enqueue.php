f637cc0e66712a14a26920e05a98c817f8c711bc
azeezs2012 <azeezs2012@gmail.com>
2022-12-20 12:28:27 +0530

    update not to remove critical css

--- includes/modules/css/critical-css/CriticalCSS_Enqueue.php
+++ includes/modules/css/critical-css/CriticalCSS_Enqueue.php
@@ -203,7 +203,7 @@ class CriticalCSS_Enqueue
                 "['mousemove', 'touchstart', 'keydown'].forEach(function (event) { var listener = function () { setTimeout(function (){ let element = document.getElementById('rapidload-critical-css'); if(element){ element.remove();} }, 1000); removeEventListener(event, listener) }; addEventListener(event, listener);});");
 
             $node->setAttribute('type', 'text/javascript');
-            $body->appendChild($node);
+            //$body->appendChild($node);
 
         }
 
