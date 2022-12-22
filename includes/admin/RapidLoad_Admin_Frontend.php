1e1a8ae179c830de33177b8d0669f184f1e02e38
azeezs2012 <azeezs2012@gmail.com>
2022-12-22 11:21:17 +0530

    update version

--- includes/admin/RapidLoad_Admin_Frontend.php
+++ includes/admin/RapidLoad_Admin_Frontend.php
@@ -85,7 +85,7 @@ class RapidLoad_Admin_Frontend
             $wp_admin_bar->add_node( array(
                 'id'    => 'rapidload-clear-cache',
                 'title' => '<span class="ab-label">' . __( 'Remove All', 'remove_all' ) . '</span>',
-                'href'  => admin_url( 'options-general.php?page=uucss&action=rapidload_purge_all' ),
+                'href'  => admin_url( 'admin.php?page=rapidload&action=rapidload_purge_all' ),
                 'meta'  => array( 'class' => 'rapidload-clear-all' ),
                 'parent' => 'rapidload'
             ));
