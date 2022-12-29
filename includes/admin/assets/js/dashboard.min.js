d44432dd804773fe17a02c11b138aa77876e57c2
azeezs2012 <azeezs2012@gmail.com>
2022-12-29 12:00:54 +0530

    update min js

---
+++ includes/admin/assets/js/dashboard.min.js
@@ -0,0 +1,1 @@
+(function($){$(document).ready(function(){$("#rapidload-dashboard input.rapidload-modules").change(function(){wp.ajax.post("rapidload_module_activation",{module:$(this).val(),activate:this.checked?"on":"off"}).then(function(i){window.location.reload()})})})})(jQuery);
