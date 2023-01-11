0028d9d156b4b68442ed72686f23d7aa05e243e3
Shakeel <shakeel@freshpixl.com>
2023-01-11 12:05:10 +0530

    onboard view

--- includes/admin/on-board/on-board-view/src/router/index.js
+++ includes/admin/on-board/on-board-view/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import onboard from '../views/onboard.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
