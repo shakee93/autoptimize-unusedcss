02917b3482284180d2d36d178b0c895c3cf379bf
shakee93 <shakee.zats@gmail.com>
2023-06-22 18:21:08 +0530

    fix: rename the root element

--- includes/admin/page-optimizer/src/main.tsx
+++ includes/admin/page-optimizer/src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "app/app";
 
 import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root') as HTMLDivElement;
+const container = document.getElementById('rapidload-page-optimizer') as HTMLDivElement;
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 root.render(<App />);
