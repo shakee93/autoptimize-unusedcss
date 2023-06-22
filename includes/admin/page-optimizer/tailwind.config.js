f7ef33a80eabe00a92bd1925cd6021fa0bb75511
shakee93 <shakee.zats@gmail.com>
2023-06-22 18:18:21 +0530

    fix: tailwind styles

--- includes/admin/page-optimizer/tailwind.config.js
+++ includes/admin/page-optimizer/tailwind.config.js
@@ -1,4 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.tsx"],
-  plugins: [require("@tailwindcss/forms")],
-};
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
