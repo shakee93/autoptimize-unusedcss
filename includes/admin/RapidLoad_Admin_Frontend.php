95d461dc72e2fc771cd67208e4cb151190a35561
shakeeb <shakee.zats@gmail.com>
2023-07-07 14:29:55 +0530

    fix not to display rapidlaod menu for customers and subscriber users

--- includes/admin/RapidLoad_Admin_Frontend.php
+++ includes/admin/RapidLoad_Admin_Frontend.php
@@ -92,23 +92,36 @@ class RapidLoad_Admin_Frontend
 
         if(apply_filters('rapidload/tool-bar-menu',true)){
 
-            $wp_admin_bar->add_node( array(
-                'id'    => 'rapidload',
-                'title' => '<img src="'. UUCSS_PLUGIN_URL .'/assets/images/logo-icon-light.svg" alt="">'.__( 'RapidLoad', 'rapidload' ),
-                'href'  => admin_url( 'admin.php?page=rapidload' ),
-                'meta'  => array( 'class' => 'bullet-green rapidload ab-item' ),
-            ));
+            $current_user = wp_get_current_user();
 
-            $wp_admin_bar->add_node( array(
-                'id'    => 'rapidload-clear-cache',
-                'title' => '<span class="ab-label">' . __( 'Clear CSS/JS Optimizations', 'clear_optimization' ) . '</span>',
-                //'href'  => admin_url( 'admin.php?page=rapidload&action=rapidload_purge_all' ),
-                'href'   => wp_nonce_url( add_query_arg( array(
-                    '_action' => 'rapidload_purge_all',
-                ) ), 'uucss_nonce', 'nonce' ),
-                'meta'  => array( 'class' => 'rapidload-clear-all', 'title' => 'RapidLoad will clear all the cached files' ),
-                'parent' => 'rapidload'
-            ));
+            if(!$current_user){
+                return;
+            }
+
+            $user_role = $current_user->roles[0];
+
+            if ( $user_role !== 'customer' && $user_role !== 'subscriber' ) {
+
+                $wp_admin_bar->add_node( array(
+                    'id'    => 'rapidload',
+                    'title' => '<img src="'. UUCSS_PLUGIN_URL .'assets/images/logo-icon-light.svg" alt="">'.__( 'RapidLoad', 'rapidload' ),
+                    'href'  => admin_url( 'admin.php?page=rapidload' ),
+                    'meta'  => array( 'class' => 'bullet-green rapidload ab-item' ),
+                ));
+
+                $wp_admin_bar->add_node( array(
+                    'id'    => 'rapidload-clear-cache',
+                    'title' => '<span class="ab-label">' . __( 'Clear CSS/JS Optimizations', 'clear_optimization' ) . '</span>',
+                    //'href'  => admin_url( 'admin.php?page=rapidload&action=rapidload_purge_all' ),
+                    'href'   => wp_nonce_url( add_query_arg( array(
+                        '_action' => 'rapidload_purge_all',
+                    ) ), 'uucss_nonce', 'nonce' ),
+                    'meta'  => array( 'class' => 'rapidload-clear-all', 'title' => 'RapidLoad will clear all the cached files' ),
+                    'parent' => 'rapidload'
+                ));
+
+            }
+
         }
 
     }
