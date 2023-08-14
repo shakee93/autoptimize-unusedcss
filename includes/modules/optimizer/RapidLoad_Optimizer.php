bfad3f2d89805a1605ff110f00d03a083a2911e5
shakeeb <shakee.zats@gmail.com>
2023-08-14 11:18:30 +0530

    add action for optimize js in optimizer class

--- includes/modules/optimizer/RapidLoad_Optimizer.php
+++ includes/modules/optimizer/RapidLoad_Optimizer.php
@@ -60,9 +60,36 @@ class RapidLoad_Optimizer
             return $urls;
         }, 10, 3);
 
-        add_action('rapidload/enqueue/optimize-js', function ($link){
+        add_action('rapidload/enqueue/optimize-js', function ($link, $job, $strategy){
+
+            $options = $strategy == "mobile" ? $job->get_mobile_options() : $job->get_desktop_options();
+
+            if(isset($options['individual-file-actions'])){
+
+                foreach ($options['individual-file-actions'] as $option){
+
+                    if(isset($option->url) && gettype($option->url) == "object" && isset($option->url->url) && isset($option->url->file_type) && $option->url->file_type == "js"){
+
+                        if(isset($option->action) && gettype($option->action) == "object" && isset($option->action) && isset($option->action->value)){
+
+                            if(isset($link->src)){
+
+                                if($option->action->value == "delay"){
+                                    if (preg_match('/.*googletagmanager\.com\/gtag\/.*/', $link->src)) {
+
+                                    }
+                                }
 
 
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
 
         }, 10 , 1);
     }
