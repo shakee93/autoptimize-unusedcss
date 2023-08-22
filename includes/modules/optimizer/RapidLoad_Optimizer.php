5fc8c196bd4d32e54aab6c7cfd22ecff37c80ebe
shakeeb <shakee.zats@gmail.com>
2023-08-22 16:39:37 +0530

    update live url

--- includes/modules/optimizer/RapidLoad_Optimizer.php
+++ includes/modules/optimizer/RapidLoad_Optimizer.php
@@ -150,7 +150,7 @@ class RapidLoad_Optimizer
 
             $api = new RapidLoad_Api();
 
-            $url = 'https://stackoverflow.com';//isset($_REQUEST['url']) ? $_REQUEST['url'] : site_url();
+            $url = isset($_REQUEST['url']) ? $_REQUEST['url'] : site_url();
 
             $result = $api->post('page-speed', [
                 'url' => $url,
