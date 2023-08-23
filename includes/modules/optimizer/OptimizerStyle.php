7958abefd5d5b776faad94588c42bdfe4215b848
shakeeb <shakee.zats@gmail.com>
2023-08-23 14:59:52 +0530

    update css file actions

--- includes/modules/optimizer/OptimizerStyle.php
+++ includes/modules/optimizer/OptimizerStyle.php
@@ -4,5 +4,24 @@ class OptimizerStyle
 {
     public function __construct(){
 
+        add_action('rapidload/enqueue/before-optimize-css', function ($sheet, $job_data, $strategy){
+
+            $options = $strategy === "mobile" ? $$job_data->job->get_mobile_options(true) : $job_data->job->get_desktop_options(true);
+
+            if(isset($options['individual-file-actions']) && !empty($options['individual-file-actions']) && is_array($options['individual-file-actions'])){
+                foreach ($options['individual-file-actions'] as $file_action){
+                    if(isset($file_action->type) && $file_action->type == "css"){
+                        if( isset($file_action->url) && !filter_var($file_action->url, FILTER_VALIDATE_URL) === false){
+                            if(isset($file_action->action) && $file_action->action == "remove"){
+                                unset($sheet->rel);
+                                unset($sheet->href);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }, 10 , 3);
+
     }
 }
