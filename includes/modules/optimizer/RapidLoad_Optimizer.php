7ff61763851c644824862d6174a0c9b40a68af0a
shakeeb <shakee.zats@gmail.com>
2023-08-24 16:41:27 +0530

    update options

--- includes/modules/optimizer/RapidLoad_Optimizer.php
+++ includes/modules/optimizer/RapidLoad_Optimizer.php
@@ -170,8 +170,8 @@ class RapidLoad_Optimizer
 
             $api = new RapidLoad_Api();
 
-//            $url = isset($_REQUEST['url']) ? $_REQUEST['url'] : site_url();
-            $url = 'https://staging.rapidload.io/';
+            $url = isset($_REQUEST['url']) ? $_REQUEST['url'] : site_url();
+            //$url = 'https://staging.rapidload.io/';
 
             $result = $api->post('page-speed', [
                 'url' => $url,
