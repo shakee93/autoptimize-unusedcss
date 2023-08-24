f3f31fad99acfb2198fcd8539692569af71e8371
shakeeb <shakee.zats@gmail.com>
2023-08-24 16:18:27 +0530

    update options

--- includes/modules/css/unused-css/UnusedCSS_Enqueue.php
+++ includes/modules/css/unused-css/UnusedCSS_Enqueue.php
@@ -43,7 +43,7 @@ class UnusedCSS_Enqueue
             wp_localize_script( 'rapidload', 'rapidload', [
                 'files' => $this->files,
                 'do_not_load_original_css' => apply_filters('rapidload/frontend/do-not-load/original-css', false),
-                'options' => RapidLoad_Base::get_merged_options()
+                'options' => $this->options
             ] );
             wp_enqueue_script( 'rapidload' );
 
