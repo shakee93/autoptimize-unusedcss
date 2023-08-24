6e6f68c54b97ed4f94c30c21535da19f4361af61
shakeeb <shakee.zats@gmail.com>
2023-08-24 16:45:18 +0530

    update options

--- includes/modules/optimizer/OptimizerJS.php
+++ includes/modules/optimizer/OptimizerJS.php
@@ -12,12 +12,13 @@ class OptimizerJS
 
             $options = $strategy === "mobile" ? $job->get_mobile_options(true) : $job->get_desktop_options(true);
 
-            $link->{'optimizer-handled'} = true;
+
 
             if(isset($options['individual-file-actions']) && !empty($options['individual-file-actions']) && is_array($options['individual-file-actions'])){
                 foreach ($options['individual-file-actions'] as $file_action){
                     if(isset($file_action->type) && $file_action->type == "js"){
                         if( isset($file_action->url) && !filter_var($file_action->url, FILTER_VALIDATE_URL) === false){
+                            $link->{'optimizer-handled'} = true;
                             if(isset($file_action->action)){
                                 switch ($file_action->action){
                                     case 'defer' : {
