57a26cb4c975202ea074d92a4411055fb65eeb82
shakeeb <shakee.zats@gmail.com>
2023-08-24 13:55:44 +0530

    update file action save meta

--- includes/modules/optimizer/RapidLoad_Optimizer.php
+++ includes/modules/optimizer/RapidLoad_Optimizer.php
@@ -282,7 +282,7 @@ class RapidLoad_Optimizer
 
                     }
 
-                    if(!$found && isset( $fileaction->meta) && isset($fileaction->action) && isset($fileaction->action->value)){
+                    if(!$found && isset( $fileaction->meta) && isset($fileaction->action) && isset($fileaction->action->value) && $fileaction->action->value != "none"){
 
                         $passed_item = json_decode($fileaction->meta);
                         $passed_item->passed = true;
