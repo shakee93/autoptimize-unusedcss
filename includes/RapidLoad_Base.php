ae508ccb41adcdf3b8636aacef6ec474a8304855
shakeeb <shakee.zats@gmail.com>
2023-09-26 13:29:53 +0530

    update on board link

--- includes/RapidLoad_Base.php
+++ includes/RapidLoad_Base.php
@@ -245,7 +245,7 @@ class RapidLoad_Base
 
                 'main_action' => [
                     'key'   => 'Get Started',
-                    'value' => admin_url( 'options-general.php?page=rapidload-on-board' )
+                    'value' => admin_url( 'options-general.php?page=rapidload' )
                 ],
                 'type'        => 'warning'
             ];
@@ -723,7 +723,7 @@ class RapidLoad_Base
     public static function display_get_start_link() {
         add_filter( 'plugin_action_links_' . plugin_basename( UUCSS_PLUGIN_FILE ), function ( $links ) {
             $_links = array(
-                '<a href="' . admin_url( 'options-general.php?page=rapidload-on-board' ) . '">Get Started <span>⚡️</span> </a>',
+                '<a href="' . admin_url( 'options-general.php?page=rapidload' ) . '">Get Started <span>⚡️</span> </a>',
             );
 
             return array_merge( $_links, $links );
