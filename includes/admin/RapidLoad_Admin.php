139e3e47e6031491eead0903853ce19c20022e57
shakeeb <shakee.zats@gmail.com>
2023-10-04 12:31:47 +0530

    update titan_checklist_cron

--- includes/admin/RapidLoad_Admin.php
+++ includes/admin/RapidLoad_Admin.php
@@ -80,7 +80,30 @@ class RapidLoad_Admin
 
         self::verify_nonce();
 
-        $status = get_option("cron_check_rapidload_success","0");
+        if ( defined( 'DISABLE_WP_CRON' ) && DISABLE_WP_CRON ) {
+            wp_send_json_error( 'The DISABLE_WP_CRON constant is set to true. WP-Cron spawning is disabled.' );
+        }
+
+        if ( defined( 'ALTERNATE_WP_CRON' ) && ALTERNATE_WP_CRON ) {
+            wp_send_json_error( 'The ALTERNATE_WP_CRON constant is set to true. WP-Cron spawning is not asynchronous.' );
+        }
+
+        $spawn = self::get_cron_spawn();
+
+        if ( is_wp_error( $spawn ) ) {
+            wp_send_json_error( sprintf( 'WP-Cron spawn failed with error: %s', $spawn->get_error_message() ) );
+        }
+
+        $code    = wp_remote_retrieve_response_code( $spawn );
+        $message = wp_remote_retrieve_response_message( $spawn );
+
+        if ( 200 === $code ) {
+            wp_send_json_success( 'WP-Cron spawning is working as expected.' );
+        } else {
+            wp_send_json_error( sprintf( 'WP-Cron spawn returned HTTP status code: %1$s %2$s', $code, $message ) );
+        }
+
+        /*$status = get_option("cron_check_rapidload_success","0");
 
         if($status != "1"){
 
@@ -92,8 +115,34 @@ class RapidLoad_Admin
 
         }
 
-        wp_send_json_success(true);
+        wp_send_json_success(true);*/
 
+    }
+
+    public function get_cron_spawn() {
+
+        $doing_wp_cron = sprintf( '%.22F', microtime( true ) );
+
+        $cron_request_array = array(
+            'url'  => site_url( 'wp-cron.php?doing_wp_cron=' . $doing_wp_cron ),
+            'key'  => $doing_wp_cron,
+            'args' => array(
+                'timeout'   => 3,
+                'blocking'  => true,
+                // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- Calling native WordPress hook.
+                'sslverify' => apply_filters( 'https_local_ssl_verify', true ),
+            ),
+        );
+
+        // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- Calling native WordPress hook.
+        $cron_request = apply_filters( 'cron_request', $cron_request_array );
+
+        # Enforce a blocking request in case something that's hooked onto the 'cron_request' filter sets it to false
+        $cron_request['args']['blocking'] = true;
+
+        $result = wp_remote_post( $cron_request['url'], $cron_request['args'] );
+
+        return $result;
     }
 
     public function rapidload_titan_feedback(){
