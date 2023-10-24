a5a5b34dbea5124f710a19ec50830a66084ed9c5
Shakeel <shakeel@freshpixl.com>
2023-10-24 13:22:15 +0530

    update databse model

--- includes/admin/frontend/src/views/dashboard.vue
+++ includes/admin/frontend/src/views/dashboard.vue
@@ -362,8 +362,8 @@
 
 <!--    <popupModel v-if="popupModel && !welcomeModel" @dont="handleDont" @cancel="handleCancel" :default="license_information.name"/>-->
     <howtoUse v-if="howtouse" @cancel="howtouse=false" @goto="gotoHome"/>
-    <welcome v-if="welcomeModel" @start="startAnalyzing" @cancel="welcomeModel = false" />
-    <update v-if="db_tobe_updated" @update="updateDatabase" @cancel="db_tobe_updated = false" :message="this.updateError"/>
+    <welcome v-if="welcomeModel && !db_tobe_updated" @start="startAnalyzing" @cancel="welcomeModel = false" />
+    <update v-if="db_tobe_updated && !welcomeModel" @update="updateDatabase" @cancel="db_tobe_updated = false" :message="this.updateError"/>
 
   </main>
 </template>
