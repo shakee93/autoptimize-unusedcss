2ad7e67503d58a3abb4492226a7db43859ea142f
shakeeb <shakee.zats@gmail.com>
2023-10-24 12:05:22 +0530

    update db to be updated variable

--- includes/RapidLoad_Base.php
+++ includes/RapidLoad_Base.php
@@ -322,7 +322,8 @@ class RapidLoad_Base
                 'activation_url' => self::activation_url('authorize' ),
                 'onboard_activation_url' => self::onboard_activation_url('authorize' ),
                 'app_url' => defined('UUCSS_APP_URL') ? trailingslashit(UUCSS_APP_URL) : 'https://app.rapidload.io/',
-                'total_jobs' => RapidLoad_DB::get_total_job_count()
+                'total_jobs' => RapidLoad_DB::get_total_job_count(),
+                'db_tobe_updated' => !RapidLoad_DB::$current_version < 1.6
             );
             wp_localize_script( 'uucss_global_admin_script', 'uucss_global', $data );
             wp_enqueue_script( 'uucss_global_admin_script' );
