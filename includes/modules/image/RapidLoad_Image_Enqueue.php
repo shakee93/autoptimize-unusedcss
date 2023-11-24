2823c7d3ccbb817290c6f2675b3de0a5cac331b5
shakeeb <shakee.zats@gmail.com>
2023-11-24 10:53:27 +0530

    update option for exclude from modern images

--- includes/modules/image/RapidLoad_Image_Enqueue.php
+++ includes/modules/image/RapidLoad_Image_Enqueue.php
@@ -82,6 +82,10 @@ class RapidLoad_Image_Enqueue
                     continue;
                 }
 
+                if($this->is_file_excluded($img->{$attribute['attr']}, 'uucss_exclude_images_from_modern_images')){
+                    continue;
+                }
+
                 $url = $this->extractUrl($img->{$attribute['attr']});
 
                 $urlExt = pathinfo($url, PATHINFO_EXTENSION);
