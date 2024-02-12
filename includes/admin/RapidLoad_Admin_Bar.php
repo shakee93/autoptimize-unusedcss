03017ad5f5ce1d56243c535e3ae64364cf41a7e6
shakeeb <shakee.zats@gmail.com>
2024-02-12 13:30:24 +0530

    add license key to be access by optimizer

--- includes/admin/RapidLoad_Admin_Bar.php
+++ includes/admin/RapidLoad_Admin_Bar.php
@@ -114,7 +114,7 @@ class RapidLoad_Admin_Bar {
             'api_root' => defined('UUCSS_API_URL') ? UUCSS_API_URL : 'https://api.rapidload.io/api/v1',
             'enable_entire_site' => RapidLoad_DB::get_optimization_count() < 2,
             'rest_url' => RapidLoadRestApi::rest_url(),
-            'licence_key' => RapidLoad_Base::get_license_key()
+            'license_key' => RapidLoad_Base::get_license_key()
         );
 
         wp_localize_script( 'rapidload_page_optimizer', 'rapidload_optimizer', $data );
