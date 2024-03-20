737535100689cd295fa07d4769f328f780fd2dad
shakee93 <shakee.zats@gmail.com>
2024-03-20 14:41:05 +0530

    fix: Speed settings button broken in small window

--- includes/admin/page-optimizer/src/app/page-optimizer/spaces/Performance.tsx
+++ includes/admin/page-optimizer/src/app/page-optimizer/spaces/Performance.tsx
@@ -139,7 +139,7 @@ const Performance = () => {
 
                     onClick={() => dispatch(setCommonState('activeTab', 'configurations'))}
                     className={cn(
-                        `dark:bg-brand-930/90 bg-brand-0 border-2 border-transparent rounded-[20px] cursor-pointer w-[200px]  flex items-center gap-2 px-5 py-3 text-sm font-medium`,
+                        `dark:bg-brand-930/90 bg-brand-0 border-2 border-transparent rounded-[20px] cursor-pointer w-[200px] flex items-center gap-2 px-3 py-3 text-sm font-medium`,
                         activeTab === 'configurations' ? "font-medium " : "text-brand-500 dark:hover:text-brand-300"
                     )}
                 > <BoltIcon className='w-4 rounded-[15px]'/>  Speed Settings</div>
