a6ee37e3bca16a32563d0bdcda2082d5aef2b0e2
shakeeb <shakee.zats@gmail.com>
2024-05-15 12:08:45 +0530

    update define constants with checking defined

--- constants.php
+++ constants.php
@@ -4,10 +4,30 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'RAPIDLOAD_PLUGIN_DIR', __DIR__ );
-define( 'RAPIDLOAD_SETTINGS_DIR', WP_CONTENT_DIR . '/settings/rapidload' );
-define( 'RAPIDLOAD_CACHE_DIR', WP_CONTENT_DIR . '/cache/rapidload-cache' );
-define( 'RAPIDLOAD_CONSTANT_FILE', __DIR__ . '/constants.php' );
-define( 'RAPIDLOAD_INDEX_FILE', ABSPATH . 'index.php' );
-define( 'UUCSS_VERSION', '2.2.16' );
-define( 'UUCSS_CACHE_CHILD_DIR', '/cache/rapidload/' );
+if ( ! defined( 'RAPIDLOAD_PLUGIN_DIR' ) ) {
+    define( 'RAPIDLOAD_PLUGIN_DIR', __DIR__ );
+}
+
+if ( ! defined( 'RAPIDLOAD_SETTINGS_DIR' ) ) {
+    define( 'RAPIDLOAD_SETTINGS_DIR', WP_CONTENT_DIR . '/settings/rapidload' );
+}
+
+if ( ! defined( 'RAPIDLOAD_CACHE_DIR' ) ) {
+    define( 'RAPIDLOAD_CACHE_DIR', WP_CONTENT_DIR . '/cache/rapidload-cache' );
+}
+
+if ( ! defined( 'RAPIDLOAD_CONSTANT_FILE' ) ) {
+    define( 'RAPIDLOAD_CONSTANT FILE', __DIR__ . '/constants.php' );
+}
+
+if ( ! defined( 'RAPIDLOAD_INDEX_FILE' ) ) {
+    define( 'RAPIDLOAD_INDEX_FILE', ABSPATH . 'index.php' );
+}
+
+if ( ! defined( 'UUCSS_VERSION' ) ) {
+    define( 'UUCSS_VERSION', '2.2.16' );
+}
+
+if ( ! defined( 'UUCSS_CACHE_CHILD_DIR' ) ) {
+    define( 'UUCSS_CACHE_CHILD_DIR', '/cache/rapidload/' );
+}
