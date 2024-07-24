622001b6d745e629f356c45f6884f2c00f02b744
shakeeb <shakee.zats@gmail.com>
2024-07-24 13:27:24 +0530

    add wp cli command to connect license

---
+++ includes/RapidLoad_CLI_Command.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once 'RapidLoad_Utils.php';
+
+if (defined('WP_CLI') && WP_CLI) {
+
+    class RapidLoad_CLI_Command
+    {
+        use RapidLoad_Utils;
+
+        public function connect_license($args, $assoc_args) {
+            list($license_key) = $args;
+
+            $uucss_api         = new RapidLoad_Api();
+            $uucss_api->apiKey = $license_key;
+            $results           = $uucss_api->post( 'connect', [ 'url' => $this->transform_url(get_site_url()), 'type' => 'wordpress' ] );
+
+            if ( $uucss_api->is_error( $results ) ) {
+                if(isset($results->errors) && isset($results->errors[0])){
+                    WP_CLI::error("License Key," . $results->errors[0]->detail . "!");
+                }else{
+                    WP_CLI::error("License Key verification fail");
+                }
+            }
+
+            WP_CLI::success("License Key connected , $license_key!");
+        }
+
+    }
+
+    WP_CLI::add_command('rapidload', 'RapidLoad_CLI_Command');
+}
