d98d234dae9d8f7585b85cd978e7f7c3e8147202
shakeeb <shakee.zats@gmail.com>
2024-09-17 17:03:42 +0530

    feat: minify google fonts inline style

--- includes/modules/font/RapidLoad_Font_Enqueue.php
+++ includes/modules/font/RapidLoad_Font_Enqueue.php
@@ -174,7 +174,9 @@ class RapidLoad_Font_Enqueue
                     if($this->font_handler){
                         $content = $this->font_handler->add_display_swap_to_inline_styles($content);
                     }
-                    $inline_style_content = sprintf('<style id="google-font-%s">%s</style>', $version, $content);
+                    $minifier = new \MatthiasMullie\Minify\CSS();
+                    $minifier->add($content);
+                    $inline_style_content = sprintf('<style id="google-font-%s">%s</style>', $version, $minifier->minify());
                     $title_content = $this->dom->find( 'title' )[0]->outertext;
                     $this->dom->find( 'title' )[0]->outertext = $title_content . $inline_style_content;
                 }else{
