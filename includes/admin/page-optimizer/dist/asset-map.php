7455588058793334fb12f5e387009a57c8a0af79
Shakeel <shakeel@freshpixl.com>
2024-09-25 16:29:15 +0530

    dashboard v3 general settings updates

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -4,13 +4,13 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
     'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
-    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
+    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.CGiuVbc_.css',
-    'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js',
     'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js',
-    'assets/index.js' => 'assets/index.b3p-dUMa.js',
-    'assets/InitTour.js' => 'assets/InitTour.DltEN9lL.js',
-    'assets/animations.js' => 'assets/animations.CWk52Myu.js'
+    'assets/animations.js' => 'assets/animations.CWk52Myu.js',
+    'assets/index.js' => 'assets/index.DrquHRVN.js',
+    'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js',
+    'assets/InitTour.js' => 'assets/InitTour.BQNHVUf9.js'
 ]);
--- includes/admin/page-optimizer/src/app/dashboard/components/GeneralSettings.tsx
+++ includes/admin/page-optimizer/src/app/dashboard/components/GeneralSettings.tsx
@@ -1,158 +1,105 @@
-import React, {Suspense, useEffect, useState} from 'react';
-import {Textarea} from "components/ui/textarea";
-import {Checkbox} from "components/ui/checkbox";
-import {getTestModeStatus, saveGeneralSettings} from "../../../store/app/appActions";
-import useCommonDispatch from "hooks/useCommonDispatch";
-import {useAppContext} from "../../../context/app";
+import React, { useEffect, useState, FC } from 'react';
+import { Textarea } from 'components/ui/textarea';
+import { Checkbox } from 'components/ui/checkbox';
+import { saveGeneralSettings } from '../../../store/app/appActions';
+import useCommonDispatch from 'hooks/useCommonDispatch';
+import { useAppContext } from '../../../context/app';
 
-interface GeneralOptions {
-    uucss_excluded_links?: string[];
-    rapidload_minify_html?: boolean;
-    uucss_query_string?: boolean;
-    preload_internal_links?: boolean;
-    uucss_enable_debug?: boolean;
-    uucss_jobs_per_queue?: string;
-    uucss_queue_interval?: string;
-    uucss_disable_add_to_queue?: boolean;
-    uucss_disable_add_to_re_queue?: boolean;
+
+interface GeneralSettingsProps {
+    options: GeneralSettings;
 }
 
-interface ActiveModules {
-    general: {
-        options: GeneralOptions;
-    };
-}
+const defaultSettings: GeneralSettings = {
+    uucss_excluded_links: [],
+    rapidload_minify_html: false,
+    uucss_query_string: false,
+    preload_internal_links: false,
+    uucss_enable_debug: false,
+    uucss_jobs_per_queue: 0,
+    uucss_queue_interval: 0,
+    uucss_disable_add_to_queue: false,
+    uucss_disable_add_to_re_queue: false,
+};
 
-interface UucssGlobal {
-    active_modules?: ActiveModules;
-}
-
-declare global {
-    interface Window {
-        uucss_global?: UucssGlobal;
-    }
-}
-
-const GeneralSettings = () => {
+const GeneralSettings: FC = () => {
     const { dispatch } = useCommonDispatch();
-    const {options} = useAppContext();
-    const [settingsData, setSettingsData] = useState<GeneralSettings>({
-        excludeUrl: [],
-        minifyHtml: false,
-        queryString: false,
-        preloadLinks: false,
-        debugMode: false,
-        queueRunJob: 0,
-        queueRunJobInterval: 0,
-        disableAutoQueue: false,
-        disableReQueue: false,
-    });
-
-    // Function to handle checkbox state change
-    const handleCheckboxChange = (key: keyof GeneralSettings) => {
-        setSettingsData(prevSettings => ({
-            ...prevSettings,
-            [key]: !prevSettings[key]
-        }));
-    };
-
-// Function to handle "Save Settings" button click
-    const handleSaveSettings = () => {
-        console.log("Saved Settings:", settingsData);
-        dispatch(saveGeneralSettings(options,settingsData));
-    };
+    const { options } = useAppContext();
+    const [settingsData, setSettingsData] = useState<GeneralSettings>(defaultSettings);
 
     useEffect(() => {
-        console.log("General Settings: ", window.uucss_global?.active_modules?.general)
-        // const GeneralSettings = window.uucss_global?.active_modules?.general.options;
-
-        if (window.uucss_global?.active_modules?.general) {
-            const GeneralSettings = window.uucss_global.active_modules.general.options;
+        const GeneralSettings = window.uucss_global?.active_modules?.general?.options;
+        if (GeneralSettings) {
             setSettingsData({
-                excludeUrl: GeneralSettings.uucss_excluded_links || [],
-                minifyHtml: !!GeneralSettings.rapidload_minify_html,
-                queryString: !!GeneralSettings.uucss_query_string,
-                preloadLinks: !!GeneralSettings.preload_internal_links,
-                debugMode: !!GeneralSettings.uucss_enable_debug,
-                queueRunJob: parseInt(GeneralSettings.uucss_jobs_per_queue || '0'),
-                queueRunJobInterval: parseInt(GeneralSettings.uucss_queue_interval || '0'),
-                disableAutoQueue: !!GeneralSettings.uucss_disable_add_to_queue,
-                disableReQueue: !!GeneralSettings.uucss_disable_add_to_re_queue,
+                uucss_excluded_links: GeneralSettings.uucss_excluded_links || [],
+                rapidload_minify_html: !!GeneralSettings.rapidload_minify_html,
+                uucss_query_string: !!GeneralSettings.uucss_query_string,
+                preload_internal_links: !!GeneralSettings.preload_internal_links,
+                uucss_enable_debug: !!GeneralSettings.uucss_enable_debug,
+                uucss_jobs_per_queue: GeneralSettings.uucss_jobs_per_queue || 0,
+                uucss_queue_interval: GeneralSettings.uucss_queue_interval || 0,
+                uucss_disable_add_to_queue: !!GeneralSettings.uucss_disable_add_to_queue,
+                uucss_disable_add_to_re_queue: !!GeneralSettings.uucss_disable_add_to_re_queue,
             });
         }
     }, []);
 
-    return ( <>
+    const handleCheckboxChange = (key: keyof GeneralSettings) => {
+        setSettingsData(prev => ({ ...prev, [key]: !prev[key] }));
+    };
 
+    const handleSaveSettings = () => {
+        dispatch(saveGeneralSettings(options, settingsData));
+        console.log("Saved Settings:", settingsData);
+    };
 
-                <div className="content flex w-full sm:w-1/2 lg:w-full flex-col px-6">
-
-                    <div className='grid items-center'>
-                        <div className="text-sm font-semibold dark:text-brand-300">Exclude URLs</div>
-                        <div className="text-xs font-normal dark:text-brand-300 text-brand-500 mt-1">URLs that needs to be excluded from the whole RapidLoad optimization.</div>
-                        <Textarea className="mt-1 focus:outline-none focus-visible:ring-0 dark:text-brand-300 focus-visible:ring-offset-0 rounded-2xl" />
-                    </div>
-
-                        <div className='relative flex gap-2 font-medium text-base w-fit items-center py-1'>
-                            <Checkbox
-                                      checked={settingsData.minifyHtml}
-                                      onCheckedChange={(c: boolean) =>{
-                                          handleCheckboxChange('minifyHtml')
-                                      }}
-                                      />
-                            <div className='flex flex-col'>
-                                <div className='select-none cursor-pointer'>Minify Html</div>
-                                <p className={`text-sm font-normal select-none`} >Minify the html output of your pages.</p>
-                            </div>
-                        </div>
-                        <div className='relative flex gap-2 font-medium text-base w-fit items-center py-1'>
-                            <Checkbox
-                                checked={settingsData.queryString}
-                                onCheckedChange={(c: boolean) =>{
-                                    handleCheckboxChange('queryString')
-                                }}
-                            />
-                            <div className='flex flex-col'>
-                                <div className='select-none cursor-pointer'>Query String</div>
-                                <p className={`text-sm font-normal select-none`} >Identify URLs with query strings as separate URLs.</p>
-                            </div>
-                        </div>
-                        <div className='relative flex gap-2 font-medium text-base w-fit items-center py-1'>
-                            <Checkbox
-                                checked={settingsData.preloadLinks}
-                                onCheckedChange={(c: boolean) =>{
-                                    handleCheckboxChange('preloadLinks')
-                                }}
-                            />
-                            <div className='flex flex-col'>
-                                <div className='select-none cursor-pointer'>Preload Links</div>
-                                <p className={`text-sm font-normal select-none`} >Preload Links</p>
-                            </div>
-                        </div>
-                        <div className='relative flex gap-2 font-medium text-base w-fit items-center py-1'>
-                            <Checkbox
-                                checked={settingsData.debugMode}
-                                onCheckedChange={(c: boolean) =>{
-                                    handleCheckboxChange('debugMode')
-                                }}
-                            />
-                            <div className='flex flex-col'>
-                                <div className='select-none cursor-pointer'>Debug Mode</div>
-                                <p className={`text-sm font-normal select-none`} >Enable debug logs for RapidLoad.</p>
-                            </div>
-                        </div>
-
-                    <div className="flex justify-end">
-                        <button
-                            onClick={handleSaveSettings}
-                            className="mt-2 justify-center cursor-pointer transition duration-300 bg-violet-950 text-sm font-semibold text-white py-1.5 px-4 border border-violet-950  rounded-lg">
-                            Save Settings
-                        </button>
-                    </div>
-
+    const renderCheckbox = (label: string, description: string, key: keyof GeneralSettings) => {
+        const isChecked = typeof settingsData[key] === 'boolean' ? settingsData[key] : false;
+        return (
+            <div className="relative flex gap-2 font-medium text-base w-fit items-center py-1">
+                <Checkbox
+                    checked={isChecked}
+                    onCheckedChange={() => handleCheckboxChange(key)}
+                />
+                <div className="flex flex-col">
+                    <div className="select-none cursor-pointer">{label}</div>
+                    <p className="text-sm font-normal select-none">{description}</p>
                 </div>
+            </div>
+        );
+    };
 
-    </>
+
+    return (
+        <div className="content flex w-full sm:w-1/2 lg:w-full flex-col px-6">
+            <div className="grid items-center mb-4">
+                <div className="text-sm font-semibold dark:text-brand-300">Exclude URLs</div>
+                <div className="text-xs font-normal dark:text-brand-300 text-brand-500 mt-1">
+                    URLs that need to be excluded from RapidLoad optimization.
+                </div>
+                <Textarea
+                    className="mt-1 focus:outline-none focus-visible:ring-0 dark:text-brand-300 rounded-2xl"
+                    value={settingsData.uucss_excluded_links?.join('\n')}
+                    onChange={(e) =>
+                        setSettingsData({ ...settingsData, uucss_excluded_links: e.target.value.split('\n') })
+                    }
+                />
+            </div>
+
+            {renderCheckbox('Minify HTML', 'Minify the HTML output of your pages.', 'rapidload_minify_html')}
+            {renderCheckbox('Query String', 'Identify URLs with query strings as separate URLs.', 'uucss_query_string')}
+            {renderCheckbox('Preload Links', 'Preload internal links for faster navigation.', 'preload_internal_links')}
+            {renderCheckbox('Debug Mode', 'Enable debug logs for RapidLoad.', 'uucss_enable_debug')}
+
+            <div className="flex justify-end">
+                <button
+                    onClick={handleSaveSettings}
+                    className="mt-2 bg-violet-950 text-sm font-semibold text-white py-1.5 px-4 rounded-lg hover:bg-violet-900"
+                >
+                    Save Settings
+                </button>
+            </div>
+        </div>
     );
 };
 
