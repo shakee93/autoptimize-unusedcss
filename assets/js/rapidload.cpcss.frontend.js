8328a9c6dd8402de3939860e3c2ce3c19485d512
shakeeb <shakee.zats@gmail.com>
2024-09-26 16:35:15 +0530

    handle resize event to fix styles

--- assets/js/rapidload.cpcss.frontend.js
+++ assets/js/rapidload.cpcss.frontend.js
@@ -2,9 +2,9 @@
 
     var RapidLoadCPCSS = function () {
         var fired = false;
-
         var load_css = function () {
             var files = document.querySelectorAll('link[data-href]');
+            var loaded_files_count = 0;
             if (!files.length || fired) return;
 
             files.forEach(function (file) {
@@ -16,6 +16,16 @@
                 link.removeAttribute('data-media');
                 link.addEventListener('load', function () {
                     file.remove();
+                    loaded_files_count++;
+                    if(loaded_files_count === files.length){
+                        window.dispatchEvent(new Event('resize'));
+                    }
+                });
+                link.addEventListener('error', function () {
+                    loaded_files_count++
+                    if(loaded_files_count === files.length){
+                        window.dispatchEvent(new Event('resize'));
+                    }
                 });
                 file.parentNode.insertBefore(link, file.nextSibling);
             });
