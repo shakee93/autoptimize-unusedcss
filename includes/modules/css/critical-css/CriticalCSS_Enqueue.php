453d501e3353cec25c1a153e7236c2abc7058d3f
shakeeb <shakee.zats@gmail.com>
2024-10-10 12:58:17 +0530

    update norapidload tag

--- includes/modules/css/critical-css/CriticalCSS_Enqueue.php
+++ includes/modules/css/critical-css/CriticalCSS_Enqueue.php
@@ -237,7 +237,7 @@ class CriticalCSS_Enqueue
 
                 $node->setAttribute('id', 'rapidload-cpcss-frontend-js');
                 $node->setAttribute('type', 'text/javascript');
-                $node->setAttribute('norapidload','');
+                $node->setAttribute('norapidload',true);
                 $body->appendChild($node);
             }
 
