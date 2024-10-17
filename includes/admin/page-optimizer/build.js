8f8c3281f183a42ab61373befda7900a2c65a51c
shakeeb <shakee.zats@gmail.com>
2024-10-17 11:05:51 +0530

    rollback changes of env param

--- includes/admin/page-optimizer/build.js
+++ includes/admin/page-optimizer/build.js
@@ -3,12 +3,12 @@ const { execSync } = require('child_process');
 
 function buildWithSourceMaps() {
     console.log('Building with source maps...');
-    execSync('vite build', { stdio: 'inherit' });
+    execSync('GENERATE_SOURCE_MAPS=true vite build', { stdio: 'inherit' });
 }
 
 function buildWithoutSourceMaps() {
     console.log('Building without source maps...');
-    execSync('vite build', { stdio: 'inherit' });
+    execSync('GENERATE_SOURCE_MAPS=false vite build', { stdio: 'inherit' });
 }
 
 buildWithSourceMaps();
