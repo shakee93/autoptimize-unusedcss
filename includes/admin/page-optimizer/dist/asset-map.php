f77f21eb342f327888b3bae1c5637a7b342e9d0e
Shakeel <shakeel@freshpixl.com>
2024-10-23 10:56:46 +0530

    dashboard build updates

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -1,16 +1,16 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
-    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.ChlP1lpW.css',
-    'assets/InitTour.js' => 'assets/InitTour.CcVxbrgn.js',
-    'assets/index.js' => 'assets/index.DsEIalGh.js',
     'assets/animations.js' => 'assets/animations.CWk52Myu.js',
-    'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js',
-    'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js'
+    'assets/index.js' => 'assets/index.mE4loQCN.js',
+    'assets/InitTour.js' => 'assets/InitTour.DfOr4g1_.js',
+    'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js',
+    'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js'
 ]);
