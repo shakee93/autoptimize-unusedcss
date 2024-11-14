ae95abe82f5117a97adb53d4a041399251603465
shakeeb <shakee.zats@gmail.com>
2024-11-14 11:15:50 +0530

    update build files

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -1,23 +1,23 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
-    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
-    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
+    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.xjkm3Y1X.css',
+    'assets/InitTour.js' => 'assets/InitTour.BXxORP3e.js',
+    'assets/index.js' => 'assets/index.DjQaNqs3.js',
     'assets/animations.js' => 'assets/animations.BKihrwa5.js',
     'assets/core-vendor.js' => 'assets/core-vendor.BbZv-wCB.js',
     'assets/vendor.js' => 'assets/vendor.C_LqEnWa.js',
-    'assets/index.js' => 'assets/index.NXB80NEi.js',
-    'assets/InitTour.js' => 'assets/InitTour.2Zwreebc.js',
+    'assets/InitTour.BXxORP3e.map' => 'assets/InitTour.BXxORP3e.js.map',
+    'assets/index.CTzjOsUj.map' => 'assets/index.CTzjOsUj.js.map',
+    'assets/index.BQa9fPAZ.map' => 'assets/index.BQa9fPAZ.js.map',
     'assets/animations.BKihrwa5.map' => 'assets/animations.BKihrwa5.js.map',
     'assets/core-vendor.BbZv-wCB.map' => 'assets/core-vendor.BbZv-wCB.js.map',
     'assets/vendor.C_LqEnWa.map' => 'assets/vendor.C_LqEnWa.js.map',
-    'assets/index.BSHP0yfk.map' => 'assets/index.BSHP0yfk.js.map',
-    'assets/index.BQa9fPAZ.map' => 'assets/index.BQa9fPAZ.js.map',
-    'assets/InitTour.2Zwreebc.map' => 'assets/InitTour.2Zwreebc.js.map',
-    'assets/index.NXB80NEi.map' => 'assets/index.NXB80NEi.js.map'
+    'assets/index.DjQaNqs3.map' => 'assets/index.DjQaNqs3.js.map'
 ]);
