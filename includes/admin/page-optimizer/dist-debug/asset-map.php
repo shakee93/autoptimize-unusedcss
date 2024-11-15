e5651a16fe40607abd6b6683f6abab2374cb7587
Shakeel <shakeel@freshpixl.com>
2024-11-15 16:41:53 +0530

    settings status bug fixing

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -8,16 +8,16 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.ChlP1lpW.css',
-    'assets/index.js' => 'assets/index.DpB0RVvh.js',
-    'assets/InitTour.js' => 'assets/InitTour.K_pSoDWz.js',
     'assets/animations.js' => 'assets/animations.CWk52Myu.js',
     'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js',
+    'assets/index.js' => 'assets/index.CCQDQ_oO.js',
     'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js',
-    'assets/index.DHFwJVg7.map' => 'assets/index.DHFwJVg7.js.map',
-    'assets/index.JHaeFuYk.map' => 'assets/index.JHaeFuYk.js.map',
-    'assets/InitTour.K_pSoDWz.map' => 'assets/InitTour.K_pSoDWz.js.map',
+    'assets/InitTour.js' => 'assets/InitTour.BrSz7NaQ.js',
     'assets/animations.CWk52Myu.map' => 'assets/animations.CWk52Myu.js.map',
     'assets/core-vendor.a21Wwx5s.map' => 'assets/core-vendor.a21Wwx5s.js.map',
+    'assets/index.CrwMrJFU.map' => 'assets/index.CrwMrJFU.js.map',
+    'assets/index.JHaeFuYk.map' => 'assets/index.JHaeFuYk.js.map',
     'assets/vendor.C2Ge5RaU.map' => 'assets/vendor.C2Ge5RaU.js.map',
-    'assets/index.DpB0RVvh.map' => 'assets/index.DpB0RVvh.js.map'
+    'assets/InitTour.BrSz7NaQ.map' => 'assets/InitTour.BrSz7NaQ.js.map',
+    'assets/index.CCQDQ_oO.map' => 'assets/index.CCQDQ_oO.js.map'
 ]);
