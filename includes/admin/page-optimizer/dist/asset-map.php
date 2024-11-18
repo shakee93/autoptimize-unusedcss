3d74a67255ae08c702f34c4fa394515a92028719
shakeeb <shakee.zats@gmail.com>
2024-11-18 09:45:42 +0530

    update build files

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -1,16 +1,16 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
-    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
-    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
-    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
+    'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
+    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/index.css' => 'assets/index.ChlP1lpW.css',
-    'assets/InitTour.js' => 'assets/InitTour.BOwGl0NB.js',
-    'assets/index.js' => 'assets/index.BOgMaXPe.js',
-    'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js',
+    'assets/InitTour.js' => 'assets/InitTour.DaMiyxQJ.js',
+    'assets/index.js' => 'assets/index.vzULuBtB.js',
     'assets/animations.js' => 'assets/animations.CWk52Myu.js',
+    'assets/core-vendor.js' => 'assets/core-vendor.a21Wwx5s.js',
     'assets/vendor.js' => 'assets/vendor.C2Ge5RaU.js'
 ]);
