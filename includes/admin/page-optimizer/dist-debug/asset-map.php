414cc5abcd7c52b916d2dac877191859d26251f4
Shakeel <shakeel@freshpixl.com>
2024-12-11 11:44:07 +0530

    build

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -1,23 +1,23 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
-    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
-    'assets/index.css' => 'assets/index.CczqPnzM.css',
+    'assets/index.css' => 'assets/index.W_oR5wH8.css',
+    'assets/index.js' => 'assets/index.CXgbv_QX.js',
+    'assets/InitTour.js' => 'assets/InitTour.DrJCPx3W.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
     'assets/animations.js' => 'assets/animations.F1FEklSB.js',
     'assets/vendor.js' => 'assets/vendor.BX0_ezNS.js',
-    'assets/index.js' => 'assets/index.B_fwx34t.js',
-    'assets/InitTour.js' => 'assets/InitTour.Bguadlvz.js',
+    'assets/index.LhhAD4-j.map' => 'assets/index.LhhAD4-j.js.map',
+    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
+    'assets/InitTour.DrJCPx3W.map' => 'assets/InitTour.DrJCPx3W.js.map',
     'assets/core-vendor.V80Ff32-.map' => 'assets/core-vendor.V80Ff32-.js.map',
     'assets/animations.F1FEklSB.map' => 'assets/animations.F1FEklSB.js.map',
     'assets/vendor.BX0_ezNS.map' => 'assets/vendor.BX0_ezNS.js.map',
-    'assets/index.Obe01QmY.map' => 'assets/index.Obe01QmY.js.map',
-    'assets/InitTour.Bguadlvz.map' => 'assets/InitTour.Bguadlvz.js.map',
-    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
-    'assets/index.B_fwx34t.map' => 'assets/index.B_fwx34t.js.map'
+    'assets/index.CXgbv_QX.map' => 'assets/index.CXgbv_QX.js.map'
 ]);
--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -7,10 +7,10 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
-    'assets/index.css' => 'assets/index.CczqPnzM.css',
-    'assets/index.js' => 'assets/index.B_fwx34t.js',
+    'assets/index.css' => 'assets/index.W_oR5wH8.css',
+    'assets/index.js' => 'assets/index.CXgbv_QX.js',
+    'assets/InitTour.js' => 'assets/InitTour.DrJCPx3W.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
     'assets/animations.js' => 'assets/animations.F1FEklSB.js',
-    'assets/InitTour.js' => 'assets/InitTour.Bguadlvz.js',
     'assets/vendor.js' => 'assets/vendor.BX0_ezNS.js'
 ]);
