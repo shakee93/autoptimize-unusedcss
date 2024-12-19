6da9c181e7c17397cf390cc781d6b83a5d409843
shakee93 <shakee.zats@gmail.com>
2024-12-19 11:50:06 +0530

    feat: change url of AI root feat: remove hardcoded url for optimization preview

--- includes/admin/page-optimizer/src/app/onboard/components/StepOne.tsx
+++ includes/admin/page-optimizer/src/app/onboard/components/StepOne.tsx
@@ -117,7 +117,7 @@ const StepOne: React.FC<StepOneProps> = ({ onNext }) => {
                                         :
                                         <PerformanceProgressBar
                                             className={cn('max-h-44')}
-                                            performance={data ? data?.performance : 0}
+                                            performance={data ? data?.performance : 90}
                                             loading={loading}
                                         />
                                     }
@@ -151,7 +151,9 @@ const StepOne: React.FC<StepOneProps> = ({ onNext }) => {
                                             className={cn('max-h-44')}
                                             scoreClassName={"text-brand-950"}
                                             performance={performanceScore}
-                                        />
+                                        >
+                                            <span className='text-brand-950 text-xs top-10 font-normal h-10'>to 100</span>
+                                        </PerformanceProgressBar>
                                     )}
 
                                 </div>
