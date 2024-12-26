098bf58d7e55e0fc1e1fedfbf8aaa5e602c736bf
shakee93 <shakee.zats@gmail.com>
2024-12-26 17:40:10 +0530

    feat: testing diagnostics

--- includes/admin/page-optimizer/src/app/page-optimizer/spaces/Optimizations.tsx
+++ includes/admin/page-optimizer/src/app/page-optimizer/spaces/Optimizations.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import AppButton from "components/ui/app-button";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "../../../components/ui/accordion";
+
 import { AnimatePresence, m, motion } from "framer-motion"
 import useCommonDispatch from "hooks/useCommonDispatch";
 import { changeGear } from '../../../store/app/appActions';
@@ -9,6 +11,8 @@ import { optimizerData } from "../../../store/app/appSelector";
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import OptimizationsProgress from '../../../components/optimizations-progress';
 import { useAppContext } from "../../../context/app";
+import ApiService from "../../../services/api";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "components/ui/collapsible";
 
 const Steps = [
     "Analyze with Google PageSpeed",
@@ -20,12 +24,36 @@ const Steps = [
     "All Optimizations are completed",
 ];
 
+
+interface Metric {
+    score: number;
+    potentialGain: number;
+    displayValue: string;
+    metric: string;
+}
+
+interface Audit {
+    name: string;
+    score?: number;
+    metrics?: string[];
+    displayValue?: string;
+}
+
+interface Audits {
+    opportunities: any[];
+    diagnostics: any[];
+}
+
 const Optimizations = ({ }) => {
-    const { activeGear, settings } = useSelector(optimizerData);
+    const { activeGear, settings, data } = useSelector(optimizerData);
     const [activeLevel, setActiveLevel] = useState<PerformanceGear>('accelerate');
     const [isAccordionOpen, setIsAccordionOpen] = useState(true);
     const [currentStep, setCurrentStep] = useState(-1);
     const [isOptimizing, setIsOptimizing] = useState(false);
+    const [aiDiagnosisResult, setAiDiagnosisResult] = useState<any>(null);
+
+    const [diagnostics, setDiagnostics] = useState([]);
+
     const [OptimizationSteps, setOptimizationSteps] = useState(Steps);
     const { dispatch } = useCommonDispatch()
     const { options } = useAppContext()
@@ -46,11 +74,11 @@ const Optimizations = ({ }) => {
     //     const handleMessage = (event: MessageEvent) => {
     //         if (event.data.type === "RAPIDLOAD_CHECK_RESULTS") {
     //             console.log("Received data from iframe:", event.data);
-                
+
     //             // Compare received data with settings
     //             const receivedData = event.data.data;
     //             settings.forEach((setting: AuditSetting) => {
-                    
+
     //                 const mainInput = setting.inputs[0];
     //                 if (!mainInput) return;
 
@@ -75,6 +103,107 @@ const Optimizations = ({ }) => {
     //         }
     //     };
 
+    const doAnalysis = async () => {
+        setShowIframe(true)
+        const api = new ApiService(options);
+        const plugins = await api.getActivePlugins();
+
+        const metricsWithAudits = transformMetrics({
+            opportunities: data?.grouped?.opportunities.map((o: any) => ({
+                name: o.name,
+                score: o.score,
+                metrics: o.metrics.map((m: any) => m.refs.acronym),
+                settings: o.settings.map((s: any) => s),
+            })),
+            diagnostics: data?.grouped?.diagnostics.map((d: any) => ({
+                name: d.name,
+                score: d.score,
+                metrics: d.metrics.map((m: any) => m.refs.acronym),
+                settings: d.settings.map((s: any) => s),
+            })),
+        }, data?.metrics.map((m: any) => ({
+            metric: m.refs.acronym,
+            potentialGain: m.potentialGain,
+            score: m.score,
+            weight: m.weight,
+            displayValue: m.displayValue,
+        })));
+
+        const _diagnostics = Object.entries(diagnostics).map(([key, value]) => value)
+        console.log()
+
+        const input = {
+            settings: settings.map((s: any) => ({
+                ...s,
+                inputs: s.inputs.map((i: any) => ({
+                    ...i,
+                    diagnostics: i.value ? _diagnostics.filter((d: any) => d.key === i.key) : []
+                })),
+            })),
+            plugins: plugins.data,
+            report: {
+                opportunities: data?.grouped?.opportunities.map((o: any) => ({
+                    name: o.name,
+                    score: o.score,
+                    metrics: o.metrics.map((m: any) => m.refs.acronym),
+                    settings: o.settings.map((s: any) => s.name),
+                })),
+                diagnostics: data?.grouped?.diagnostics.map((d: any) => ({
+                    name: d.name,
+                    score: d.score,
+                    metrics: d.metrics.map((m: any) => m.refs.acronym),
+                    settings: d.settings.map((s: any) => s.name),
+                })),
+            },
+            // diagnostics: Object.entries(diagnostics).map(([key, value]) => value),
+        }
+
+        const result = await api.getAIDiagnosis(input)
+        setAiDiagnosisResult(result.data.diagnostics)
+        console.log(result)
+
+    }
+
+    function transformMetrics(
+        audits: Audits,
+        metrics: Metric[],
+    ): MetricWithAudits[] {
+        // Helper function to get all audits related to a specific metric
+        const getAuditsForMetric = (metricName: string): Audit[] => {
+            const relatedAudits = [
+                ...audits.opportunities.filter(audit => audit.metrics?.includes(metricName)),
+                ...audits.diagnostics.filter(audit => audit.metrics?.includes(metricName))
+            ];
+            return relatedAudits;
+        };
+
+        // Get audits with no metrics
+        const getAuditsWithNoMetrics = (): Audit[] => {
+            return [
+                ...audits.opportunities.filter(audit => !audit.metrics || audit.metrics.length === 0),
+                ...audits.diagnostics.filter(audit => !audit.metrics || audit.metrics.length === 0)
+            ];
+        };
+
+        // Group audits by metrics
+        const metricsWithAudits: MetricWithAudits[] = [
+            ...metrics.map(metric => ({
+                ...metric,
+                audits: getAuditsForMetric(metric.metric)
+            })),
+            {
+                metric: 'NOMETRIC',
+                potentialGain: 0,
+                score: 100,
+                displayValue: '-',
+                audits: getAuditsWithNoMetrics()
+            }
+        ];
+
+
+        return metricsWithAudits;
+    }
+
     useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
             if (event.data.type === "RAPIDLOAD_CHECK_RESULTS") {
@@ -84,10 +213,10 @@ const Optimizations = ({ }) => {
                 const receivedData = event.data.data;
                 const matches: any[] = [];
 
-                 settings.forEach((setting: AuditSetting) => {
+                settings.forEach((setting: AuditSetting) => {
                     const mainInput = setting.inputs[0];
                     if (!mainInput) return;
-                     Object.entries(receivedData).forEach(([category, data]: [string, any]) => {
+                    Object.entries(receivedData).forEach(([category, data]: [string, any]) => {
                         if (data.key === mainInput.key) {
                             matches.push({
                                 key: category,
@@ -106,7 +235,8 @@ const Optimizations = ({ }) => {
                         }
                     });
                 });
-                 console.log('All matches:', matches);
+
+                setDiagnostics(event.data.data)
             }
         };
 
@@ -165,11 +295,52 @@ const Optimizations = ({ }) => {
                             <span className="font-normal text-sm text-zinc-600 dark:text-brand-300">Your optimizations are complete! However, changes might not take effect due to factors like caching, conflicts with plugins or themes, or dynamic content. Let's test to ensure everything is working smoothly and identify any bottlenecks.</span>
                             <AppButton
                                 className="rounded-xl px-8 py-4"
-                                onClick={() => setShowIframe(true)}
+                                onClick={() => doAnalysis()}
                             >
                                 Run Optimization Test
                             </AppButton>
                             <span className="font-normal text-xs text-zinc-600 dark:text-brand-300">Disabled: All Optimizations needs to be completed to run the test</span>
+                        </div>
+                    </div>
+
+
+                    <div className="mt-4 bg-brand-0 rounded-2xl p-4 py-6">
+                        <h3 className="font-semibold text-lg">AI Diagnosis</h3>
+                        <div className="mt-4">
+
+                            {aiDiagnosisResult?.CriticalIssues?.map((result: any) => (
+                                <Accordion key={result?.issue} type="single" collapsible>
+                                    <AccordionItem value={result?.issue}>
+                                        <AccordionTrigger className="text-lg font-semibold text-zinc-900 dark:text-zinc-100">
+                                            {result?.issue}
+                                        </AccordionTrigger>
+                                        <AccordionContent>
+                                            <div className="space-y-4">
+                                                <p className="text-sm text-zinc-600 dark:text-zinc-300">{result?.description}</p>
+                                                <div>
+                                                    <p className="text-sm font-medium text-zinc-800 dark:text-zinc-200 mb-2">How to fix</p>
+                                                    <ul className="list-disc list-inside space-y-2">
+                                                        {result?.howToFix?.map((fix: any) => (
+                                                            <li key={fix} className="text-sm text-zinc-600 dark:text-zinc-300">{fix}</li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+                                            <Collapsible>
+                                                <CollapsibleTrigger className="text-sm font-medium text-zinc-800 dark:text-zinc-200 mb-2">
+                                                    View Raw Data
+                                                </CollapsibleTrigger>
+                                                <CollapsibleContent>
+                                                    <pre className="mt-2 p-4 bg-zinc-50 dark:bg-zinc-900 rounded-lg overflow-auto">
+                                                        {JSON.stringify(result, null, 2)}
+                                                    </pre>
+                                                </CollapsibleContent>
+                                            </Collapsible>
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
+                            ))}
                         </div>
                     </div>
 
--- includes/admin/page-optimizer/tailwind.config.js
+++ includes/admin/page-optimizer/tailwind.config.js
@@ -1,168 +1,201 @@
 /** @type {DefaultColors} */
 const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
+
 let plugin = require('tailwindcss/plugin')
 function rem2px(input, fontSize = 16) {
-  if (input == null) {
-    return input;
-  }
-  switch (typeof input) {
-    case 'object':
-      if (Array.isArray(input)) {
-        return input.map((val) => rem2px(val, fontSize));
-      }
-      const ret = {};
-      for (const key in input) {
-        ret[key] = rem2px(input[key], fontSize);
-      }
-      return ret;
-    case 'string':
-      return input.replace(
-          /(\d*\.?\d+)rem$/,
-          (_, val) => `${parseFloat(val) * fontSize}px`,
-      );
-    case 'function':
-      return eval(input.toString().replace(
-          /(\d*\.?\d+)rem/g,
-          (_, val) => `${parseFloat(val) * fontSize}px`,
-      ));
-    default:
-      return input;
-  }
+	if (input == null) {
+		return input;
+	}
+	switch (typeof input) {
+		case 'object':
+			if (Array.isArray(input)) {
+				return input.map((val) => rem2px(val, fontSize));
+			}
+			const ret = {};
+			for (const key in input) {
+				ret[key] = rem2px(input[key], fontSize);
+			}
+			return ret;
+		case 'string':
+			return input.replace(
+				/(\d*\.?\d+)rem$/,
+				(_, val) => `${parseFloat(val) * fontSize}px`,
+			);
+		case 'function':
+			return eval(input.toString().replace(
+				/(\d*\.?\d+)rem/g,
+				(_, val) => `${parseFloat(val) * fontSize}px`,
+			));
+		default:
+			return input;
+	}
 }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // prefix: 'rl-',
-  darkMode: ["class", ".rapidload-dark"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+	// prefix: 'rl-',
+	darkMode: ["class", ".rapidload-dark"],
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
 	],
-  theme: {
-    fontFamily: {
-      'sans': ['Inter Variable', ...defaultTheme.fontFamily.sans]
-    },
-    // to fix font-size: 62% problem
-    fontSize: rem2px(defaultTheme.fontSize),
-    lineHeight: rem2px(defaultTheme.lineHeight),
-    spacing: rem2px(defaultTheme.spacing),
-    borderRadius: rem2px(defaultTheme.borderRadius),
-    minWidth: rem2px(defaultTheme.minWidth),
-    width: rem2px(defaultTheme.width),
-    container: {
-      center: true,
-      padding: "1.2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      transitionDuration: {
-        400 : '400ms'
-      },
-      fontSize: {
-        xxs: ['10px', {
-          lineHeight: '1',
-        }],
-      },
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        brand: {
-          0: '#fff',
-          930: '#101012',
-          ...colors.zinc
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        'gray-350': '#C0C0C0',
-        'purple-750': '#7F54B3',
-        'zinc-930': '#101012'
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-        'rl-scale-in' : {
-          'from': {
-            opacity: 0,
-            transform: 'translateY(-5px)',
-          },
-          'to': {
-            opacity: 1,
-            transform: 'translateY(0)',
-          },
-        },
-        'rl-loading-loop' : {
-          '0%, 100%' : {
-            transform: 'translateX(-50%)'
-          },
-          '50%': {
-            transform: 'translateX(250%)'
-          }
-        },
-        'bounce-horizontal': {
-          '0%, 20%, 50%, 80%, 100%': {
-            transform: 'translateX(0)'
-          },
-          '40%': {
-            transform: 'translateX(-5px)'  // Reduced horizontal distance for subtleness
-          },
-          '60%': {
-            transform: 'translateX(3px)'   // Reduced horizontal distance for subtleness
-          }
-        }
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        'rl-scale-in': 'rl-scale-in 0.1s ease-out',
-        'rl-loading-loop': 'rl-loading-loop 1.8s ease-out infinite',
-        'bounce-horizontal': 'bounce-horizontal 1.5s infinite'
-      },
-    },
-  },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      // Add a `third` variant, ie. `third:pb-0`
-      addVariant('kids', '&>*')
-    }),
-      require("tailwindcss-animate")],
+	theme: {
+		fontFamily: {
+			sans: [
+				'Inter Variable',
+				...defaultTheme.fontFamily.sans
+			]
+		},
+		fontSize: rem2px(defaultTheme.fontSize),
+		lineHeight: rem2px(defaultTheme.lineHeight),
+		spacing: rem2px(defaultTheme.spacing),
+		borderRadius: rem2px(defaultTheme.borderRadius),
+		minWidth: rem2px(defaultTheme.minWidth),
+		width: rem2px(defaultTheme.width),
+		container: {
+			center: true,
+			padding: '1.2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			transitionDuration: {
+				'400': '400ms'
+			},
+			fontSize: {
+				xxs: [
+					'10px',
+					{
+						lineHeight: '1'
+					}
+				]
+			},
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				brand: {
+					'0': '#fff',
+					'930': '#101012',
+					...colors.zinc
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				'gray-350': '#C0C0C0',
+				'purple-750': '#7F54B3',
+				'zinc-930': '#101012'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: 0
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: 0
+					}
+				},
+				'rl-scale-in': {
+					from: {
+						opacity: 0,
+						transform: 'translateY(-5px)'
+					},
+					to: {
+						opacity: 1,
+						transform: 'translateY(0)'
+					}
+				},
+				'rl-loading-loop': {
+					'0%, 100%': {
+						transform: 'translateX(-50%)'
+					},
+					'50%': {
+						transform: 'translateX(250%)'
+					}
+				},
+				'bounce-horizontal': {
+					'0%, 20%, 50%, 80%, 100%': {
+						transform: 'translateX(0)'
+					},
+					'40%': {
+						transform: 'translateX(-5px)'
+					},
+					'60%': {
+						transform: 'translateX(3px)'
+					}
+				},
+				'accordion-down': {
+					from: {
+						height: '0'
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: '0'
+					}
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'rl-scale-in': 'rl-scale-in 0.1s ease-out',
+				'rl-loading-loop': 'rl-loading-loop 1.8s ease-out infinite',
+				'bounce-horizontal': 'bounce-horizontal 1.5s infinite',
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
+			}
+		}
+	},
+	plugins: [
+		plugin(function ({ addVariant }) {
+			// Add a `third` variant, ie. `third:pb-0`
+			addVariant('kids', '&>*')
+		}),
+		require("tailwindcss-animate"),
+	],
 }
