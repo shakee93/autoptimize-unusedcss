01704ef63e9eec6e7085e2d59a0e4dfb54ff337f
Shakeel <shakeel@freshpixl.com>
2025-01-31 14:57:02 +0530

    overlap issue fixed

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -1,8 +1,8 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
-    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
@@ -10,7 +10,7 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/index.css' => 'assets/index.BrtPb-Y0.css',
     'assets/animations.js' => 'assets/animations.CVpoelRy.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
-    'assets/index.js' => 'assets/index.BgwIgIBp.js',
-    'assets/InitTour.js' => 'assets/InitTour.DmdqsCGe.js',
-    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js'
+    'assets/InitTour.js' => 'assets/InitTour.CL9weleI.js',
+    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
+    'assets/index.js' => 'assets/index.DKIYqoC-.js'
 ]);
--- includes/admin/page-optimizer/src/app/ai-bot/index.tsx
+++ includes/admin/page-optimizer/src/app/ai-bot/index.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Chat2 from "app/ai-bot/components/Chat2";
+import { m, AnimatePresence } from "framer-motion";
 
 export default function ChatPage() {
   return (
-    <div className="flex justify-center items-center container">
+    <m.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+                ease: 'linear',
+                duration: 0.04,
+            }}
+            id='rapidload-page-optimizer-wrapper' className="flex justify-center items-center container">
       <Chat2 />
-    </div>
+    </m.div>
   );
 }
