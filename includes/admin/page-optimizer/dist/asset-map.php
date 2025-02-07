35a338608c778ec2a62058cab4362f95a2fd0469
Shakeel <shakeel@freshpixl.com>
2025-02-07 17:07:52 +0530

    build v3

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -8,9 +8,9 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.CN4vohwt.css',
-    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
-    'assets/InitTour.js' => 'assets/InitTour.sgaKKVSA.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
-    'assets/index.js' => 'assets/index.DmbRQtcE.js',
-    'assets/animations.js' => 'assets/animations.CVpoelRy.js'
+    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
+    'assets/InitTour.js' => 'assets/InitTour.DrSGKbsY.js',
+    'assets/animations.js' => 'assets/animations.CVpoelRy.js',
+    'assets/index.js' => 'assets/index.D5xVUhGp.js'
 ]);
