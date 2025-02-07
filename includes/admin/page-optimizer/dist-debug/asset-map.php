969adf61489e7f67ccc37b3e17267c3699d30956
Shakeel <shakeel@freshpixl.com>
2025-02-07 15:24:08 +0530

    home performance changes

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -1,23 +1,23 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
-    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
-    'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
+    'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/index.css' => 'assets/index.CN4vohwt.css',
-    'assets/index.js' => 'assets/index.BTDsApcf.js',
-    'assets/InitTour.js' => 'assets/InitTour.ClwN9v1e.js',
     'assets/animations.js' => 'assets/animations.CVpoelRy.js',
+    'assets/index.js' => 'assets/index.D5xVUhGp.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
     'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
-    'assets/index.CgYSpLvd.map' => 'assets/index.CgYSpLvd.js.map',
-    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
-    'assets/InitTour.ClwN9v1e.map' => 'assets/InitTour.ClwN9v1e.js.map',
+    'assets/InitTour.js' => 'assets/InitTour.DrSGKbsY.js',
     'assets/animations.CVpoelRy.map' => 'assets/animations.CVpoelRy.js.map',
+    'assets/index.CwbGp8Ah.map' => 'assets/index.CwbGp8Ah.js.map',
     'assets/core-vendor.V80Ff32-.map' => 'assets/core-vendor.V80Ff32-.js.map',
+    'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
     'assets/vendor.T3CYoFTJ.map' => 'assets/vendor.T3CYoFTJ.js.map',
-    'assets/index.BTDsApcf.map' => 'assets/index.BTDsApcf.js.map'
+    'assets/InitTour.DrSGKbsY.map' => 'assets/InitTour.DrSGKbsY.js.map',
+    'assets/index.D5xVUhGp.map' => 'assets/index.D5xVUhGp.js.map'
 ]);
