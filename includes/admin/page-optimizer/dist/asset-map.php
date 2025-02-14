c011a42b9039dbc65c7a10941dd7cb8d34066b1c
Shakeel <shakeel@freshpixl.com>
2025-02-14 11:45:47 +0530

    dark mode updates

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -7,10 +7,10 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
-    'assets/index.css' => 'assets/index.gkUZYDgM.css',
-    'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
-    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
+    'assets/index.css' => 'assets/index.D54psRY7.css',
     'assets/animations.js' => 'assets/animations.CVpoelRy.js',
-    'assets/InitTour.js' => 'assets/InitTour.D_45AJez.js',
-    'assets/index.js' => 'assets/index.Bg-stsRF.js'
+    'assets/index.js' => 'assets/index.1rM-eYZT.js',
+    'assets/InitTour.js' => 'assets/InitTour.CxZITwIQ.js',
+    'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
+    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js'
 ]);
