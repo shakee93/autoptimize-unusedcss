808d55c6a120de072402c28657c1516ca4a9999c
shakeeb <shakee.zats@gmail.com>
2025-02-19 11:05:04 +0530

    chore: update version and changelog

--- constants.php
+++ constants.php
@@ -25,7 +25,7 @@ if ( ! defined( 'RAPIDLOAD_INDEX_FILE' ) ) {
 }
 
 if ( ! defined( 'UUCSS_VERSION' ) ) {
-    define( 'UUCSS_VERSION', '3.0.2' );
+    define( 'UUCSS_VERSION', '3.0.3' );
 }
 
 if ( ! defined( 'UUCSS_CACHE_CHILD_DIR' ) ) {
