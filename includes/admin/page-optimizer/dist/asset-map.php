007961e656f910df66623e2843ae26e403b64122
Shakeel <shakeel@freshpixl.com>
2025-02-25 16:29:41 +0530

    build

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -8,9 +8,9 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.ElsU_-9h.css',
-    'assets/index.js' => 'assets/index.BA2nBowu.js',
-    'assets/InitTour.js' => 'assets/InitTour.Dq0q0X9r.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
     'assets/animations.js' => 'assets/animations.CVpoelRy.js',
+    'assets/index.js' => 'assets/index.BA2nBowu.js',
+    'assets/InitTour.js' => 'assets/InitTour.Dq0q0X9r.js',
     'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js'
 ]);
