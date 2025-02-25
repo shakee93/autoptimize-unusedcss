cec9dd953609647aacce868222f5594b07ded93b
shakeeb <shakee.zats@gmail.com>
2025-02-25 14:54:25 +0530

    update build version

--- includes/admin/page-optimizer/dist-debug/asset-map.php
+++ includes/admin/page-optimizer/dist-debug/asset-map.php
@@ -1,23 +1,23 @@
 <?php
 define('RAPIDLOAD_ASSET_MAP', [
-    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-cyrillic-wght-normal.woff2' => 'assets/inter-cyrillic-wght-normal.woff2',
-    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
     'assets/inter-greek-wght-normal.woff2' => 'assets/inter-greek-wght-normal.woff2',
     'assets/inter-vietnamese-wght-normal.woff2' => 'assets/inter-vietnamese-wght-normal.woff2',
+    'assets/inter-greek-ext-wght-normal.woff2' => 'assets/inter-greek-ext-wght-normal.woff2',
+    'assets/inter-cyrillic-ext-wght-normal.woff2' => 'assets/inter-cyrillic-ext-wght-normal.woff2',
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.ElsU_-9h.css',
-    'assets/index.js' => 'assets/index.BA2nBowu.js',
+    'assets/index.js' => 'assets/index.BgTUC-F6.js',
+    'assets/InitTour.js' => 'assets/InitTour.BaI7a9ZL.js',
     'assets/animations.js' => 'assets/animations.CVpoelRy.js',
-    'assets/InitTour.js' => 'assets/InitTour.Dq0q0X9r.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
     'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
-    'assets/index.CP1ifNrq.map' => 'assets/index.CP1ifNrq.js.map',
+    'assets/index.BcM2wwoo.map' => 'assets/index.BcM2wwoo.js.map',
+    'assets/InitTour.BaI7a9ZL.map' => 'assets/InitTour.BaI7a9ZL.js.map',
     'assets/animations.CVpoelRy.map' => 'assets/animations.CVpoelRy.js.map',
-    'assets/InitTour.Dq0q0X9r.map' => 'assets/InitTour.Dq0q0X9r.js.map',
     'assets/index.Be5yF97N.map' => 'assets/index.Be5yF97N.js.map',
     'assets/core-vendor.V80Ff32-.map' => 'assets/core-vendor.V80Ff32-.js.map',
     'assets/vendor.T3CYoFTJ.map' => 'assets/vendor.T3CYoFTJ.js.map',
-    'assets/index.BA2nBowu.map' => 'assets/index.BA2nBowu.js.map'
+    'assets/index.BgTUC-F6.map' => 'assets/index.BgTUC-F6.js.map'
 ]);
