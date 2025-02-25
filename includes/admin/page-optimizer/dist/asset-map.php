1cf2bd042201f999b305d492411fd1bcf13e16a9
Shakeel <shakeel@freshpixl.com>
2025-02-25 14:17:12 +0530

    build dark mode changes with caht fixes

--- includes/admin/page-optimizer/dist/asset-map.php
+++ includes/admin/page-optimizer/dist/asset-map.php
@@ -8,9 +8,9 @@ define('RAPIDLOAD_ASSET_MAP', [
     'assets/inter-latin-ext-wght-normal.woff2' => 'assets/inter-latin-ext-wght-normal.woff2',
     'assets/inter-latin-wght-normal.woff2' => 'assets/inter-latin-wght-normal.woff2',
     'assets/index.css' => 'assets/index.DIaXBggk.css',
+    'assets/index.js' => 'assets/index.CLyGT50i.js',
+    'assets/InitTour.js' => 'assets/InitTour.CgBVr3k4.js',
+    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js',
     'assets/core-vendor.js' => 'assets/core-vendor.V80Ff32-.js',
-    'assets/InitTour.js' => 'assets/InitTour.W_Mjlto4.js',
-    'assets/index.js' => 'assets/index.DuKuwqJC.js',
-    'assets/animations.js' => 'assets/animations.CVpoelRy.js',
-    'assets/vendor.js' => 'assets/vendor.T3CYoFTJ.js'
+    'assets/animations.js' => 'assets/animations.CVpoelRy.js'
 ]);
